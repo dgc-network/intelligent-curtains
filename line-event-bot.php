04a1417cd76b4058f8bbeb31967eb30d72280ad5
roverchen <rover.k.chen@gmail.com>
2022-07-13 16:10:09 +0800

    init

--- line-event-bot.php
+++ line-event-bot.php
@@ -20,4 +20,56 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 include_once dirname( __FILE__ ) . '/line-bot-sdk-tiny/LINEBotTiny.php';
 
+$channelAccessToken = 'ongg0SgvMZjDQlO3qHvSvGBU/JyMlz2GBiRi9t7iUBHXqZIZAioD9Im7gJ6MYLBA/Aq5BupS6HEd6U/cxDKvstGnUWPfHcQ9OEpQ3QGK44BUzAkp7s3CXP0G4h2C0/o1UO7xpmiI3RelAJhTWK8khQdB04t89/1O/w1cDnyilFU=';
+$channelSecret = '1bd1c2ac3b3a36399de32f5a83f135c0';
+
+$client = new LINEBotTiny($channelAccessToken, $channelSecret);
+
+foreach ($client->parseEvents() as $event) {
+    $getsource = $event['source'];
+    $usr_id = $getsource['userId'];
+/*
+    $response = $bot->getProfile('<userId>');
+    if ($response->isSucceeded()) {
+        $profile = $response->getJSONDecodedBody();
+        echo $profile['displayName'];
+        echo $profile['pictureUrl'];
+        echo $profile['statusMessage'];
+    }
+*/
+/*    
+    $response = wp_remote_get( 'https://api.line.me/v2/bot/profile/'.$usr_id, array(
+        'header'    => array(
+            'Authentication'    => 'Bearer '.$channelAccessToken
+        )
+    ));
+*/
+    switch ($event['type']) {
+        case 'message':
+            $message = $event['message'];
+            switch ($message['type']) {
+                case 'text':
+                    // start my codes from here
+
+                    $client->replyMessage([
+                        'replyToken' => $event['replyToken'],
+                        'messages' => [
+                            [
+                                'type' => 'text',
+                                'text' => $usr_id.':'.$message['text']
+                            ]
+                        ]
+                    ]);
+                    break;
+                default:
+                    error_log('Unsupported message type: ' . $message['type']);
+                    break;
+            }
+            break;
+        default:
+            error_log('Unsupported event type: ' . $event['type']);
+            break;
+    }
+    
+};
 ?>
