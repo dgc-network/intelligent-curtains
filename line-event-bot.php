ce4f6a28239b157b3d0df5c9788cd5c35eeef843
roverchen <rover.k.chen@gmail.com>
2022-07-13 16:58:54 +0800

    $insert_id = $wpdb->insert($table, $data, $format);

--- line-event-bot.php
+++ line-event-bot.php
@@ -23,27 +23,30 @@ include_once dirname( __FILE__ ) . '/line-bot-sdk-tiny/LINEBotTiny.php';
 $channelAccessToken = 'ongg0SgvMZjDQlO3qHvSvGBU/JyMlz2GBiRi9t7iUBHXqZIZAioD9Im7gJ6MYLBA/Aq5BupS6HEd6U/cxDKvstGnUWPfHcQ9OEpQ3QGK44BUzAkp7s3CXP0G4h2C0/o1UO7xpmiI3RelAJhTWK8khQdB04t89/1O/w1cDnyilFU=';
 $channelSecret = '1bd1c2ac3b3a36399de32f5a83f135c0';
 
+create_tables();
+
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 
 foreach ($client->parseEvents() as $event) {
     $getsource = $event['source'];
     $usr_id = $getsource['userId'];
-/*
-    $response = $bot->getProfile('<userId>');
-    if ($response->isSucceeded()) {
-        $profile = $response->getJSONDecodedBody();
-        echo $profile['displayName'];
-        echo $profile['pictureUrl'];
-        echo $profile['statusMessage'];
-    }
-*/
-/*    
-    $response = wp_remote_get( 'https://api.line.me/v2/bot/profile/'.$usr_id, array(
-        'header'    => array(
-            'Authentication'    => 'Bearer '.$channelAccessToken
-        )
-    ));
-*/
+
+    global $wpdb;
+    $table = $wpdb->prefix.'eventLogs';
+    $data = array(
+        'event_type' => $event['type'],
+        'event_timestamp' => $event['timestamp'],
+        'event_source' => $event['source'],
+        'event_replyToken' => $event['replyToken'],
+        'event_mode' => $event['mode'],
+        'webhookEventId' => $event['webhookEventId'],
+        'isRedelivery' => $event['deliveryContext']['isRedelivery'],
+        //'event_object' => $event['message'],
+    );
+    //$format = array('%s', '%d', '%s', '%s');
+    //$insert_id = $wpdb->insert($table, $data, $format);
+    $insert_id = $wpdb->insert($table, $data);
+
     switch ($event['type']) {
         case 'message':
             $message = $event['message'];
@@ -69,7 +72,81 @@ foreach ($client->parseEvents() as $event) {
         default:
             error_log('Unsupported event type: ' . $event['type']);
             break;
-    }
-    
+    }    
 };
+
+function create_tables() {
+        
+    global $wpdb;
+    $charset_collate = $wpdb->get_charset_collate();
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+
+    $sql = "CREATE TABLE `{$wpdb->prefix}eventLogs` (
+        event_id int NOT NULL AUTO_INCREMENT,
+        event_type varchar(255),
+        event_timestamp int,
+        event_source varchar(255),
+        event_replyToken varchar(255),
+        event_mode varchar(255),
+        webhookEventId varchar(255),
+        isRedelivery boolean,
+        event_object varchar(255),
+        PRIMARY KEY  (event_id)
+    ) $charset_collate;";        
+    dbDelta($sql);
+
+}
+/*
+{
+    "destination": "xxxxxxxxxx",
+    "events": [
+      {
+        "type": "message",
+        "message": {
+          "type": "text",
+          "id": "14353798921116",
+          "text": "Hello, world"
+        },
+        "timestamp": 1625665242211,
+        "source": {
+          "type": "user",
+          "userId": "U80696558e1aa831..."
+        },
+        "replyToken": "757913772c4646b784d4b7ce46d12671",
+        "mode": "active",
+        "webhookEventId": "01FZ74A0TDDPYRVKNK77XKC3ZR",
+        "deliveryContext": {
+          "isRedelivery": false
+        }
+      },
+      {
+        "type": "follow",
+        "timestamp": 1625665242214,
+        "source": {
+          "type": "user",
+          "userId": "Ufc729a925b3abef..."
+        },
+        "replyToken": "bb173f4d9cf64aed9d408ab4e36339ad",
+        "mode": "active",
+        "webhookEventId": "01FZ74ASS536FW97EX38NKCZQK",
+        "deliveryContext": {
+          "isRedelivery": false
+        }
+      },
+      {
+        "type": "unfollow",
+        "timestamp": 1625665242215,
+        "source": {
+          "type": "user",
+          "userId": "Ubbd4f124aee5113..."
+        },
+        "mode": "active",
+        "webhookEventId": "01FZ74B5Y0F4TNKA5SCAVKPEDM",
+        "deliveryContext": {
+          "isRedelivery": false
+        }
+      }
+    ]
+  }
+*/  
 ?>
