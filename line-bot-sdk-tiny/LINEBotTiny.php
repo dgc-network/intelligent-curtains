1b24521acd46bbf04b07b41989c0b210b9992f20
roverchen <rover.k.chen@gmail.com>
2022-07-14 13:29:31 +0800

    {$userId}

--- line-bot-sdk-tiny/LINEBotTiny.php
+++ line-bot-sdk-tiny/LINEBotTiny.php
@@ -159,7 +159,7 @@ class LINEBotTiny
             ],
         ]);
 
-        $response = file_get_contents('https://api.line.me/v2/bot/profile/{$userId}', false, $context);
+        $response = file_get_contents('https://api.line.me/v2/bot/profile/'.$userId, false, $context);
         if (strpos($http_response_header[0], '200') === false) {
             error_log('Request failed: ' . $response);
         }
