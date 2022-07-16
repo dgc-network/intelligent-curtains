eb022340b6fad5ffc36f8c012540075fc5dfabf9
roverchen <rover.k.chen@gmail.com>
2022-07-16 21:33:58 +0800

    filePath

--- includes/class-event-bot.php
+++ includes/class-event-bot.php
@@ -205,8 +205,8 @@ if (!class_exists('event_bot')) {
                     case 'image':
                         $response = self::line_bot_sdk()->getContent($message['id']);
                         //$display_message = json_encode($response);
-                        $display_message = $response;
-                        //$display_message = $message['id'];
+                        //$display_message = $response;
+                        $display_message = $message['id'];
                         //$display_message = $message['contentProvider']['type'];
                         break;
                     default:
