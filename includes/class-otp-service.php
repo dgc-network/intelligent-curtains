d87a3f86ac5883832e32a981b5229105cb0fe6d1
Rover Chen <rover.k.chen@gmail.com>
2022-09-13 06:08:24 +0800

    isset($_GET['curtain_product_id'])

--- includes/class-otp-service.php
+++ includes/class-otp-service.php
@@ -10,79 +10,15 @@ if (!class_exists('otp_service')) {
          * Class constructor
          */
         public function __construct() {
-            //add_shortcode('wporg-shortcode', __CLASS__ . '::wporg_shortcode');
             add_shortcode('product-info', __CLASS__ . '::product_info');
-            add_shortcode('issue-otp', __CLASS__ . '::issue_otp');
             add_shortcode('serial-number-list', __CLASS__ . '::list_serial_number');
             add_shortcode('curtain-product-list', __CLASS__ . '::list_curtain_product');
             add_shortcode('curtain-user-list', __CLASS__ . '::list_curtain_user');
             self::create_tables();
             //self::delete_records();
         }
-/*
-        public function line_bot_sdk() {
-            $channelAccessToken = '';
-            $channelSecret = '';
-            $plugin_dir = WP_PLUGIN_DIR . '/line-event-bot';
-            if (file_exists($plugin_dir . '/line-bot-sdk-tiny/config.ini')) {
-                $config = parse_ini_file($plugin_dir . "/line-bot-sdk-tiny/config.ini", true);
-                if ($config['Channel']['Token'] == null || $config['Channel']['Secret'] == null) {
-                    error_log("config.ini 配置檔未設定完全！", 0);
-                } else {
-                    $channelAccessToken = $config['Channel']['Token'];
-                    $channelSecret = $config['Channel']['Secret'];
-                }
-            }
-            $client = new LINEBotTiny($channelAccessToken, $channelSecret);
-            return $client;
-        }
-*/
-        /**
-         * /**
-         * The [wporg] shortcode.
-         *
-         * Accepts a title and will display a box.
-         *
-         * @param array  $atts    Shortcode attributes. Default empty.
-         * @param string $content Shortcode content. Default null.
-         * @param string $tag     Shortcode tag (name). Default empty.
-         * @return string Shortcode output.
-         */
-        function wporg_shortcode( $atts = [], $content = null, $tag = '' ) {
-            // normalize attribute keys, lowercase
-            $atts = array_change_key_case( (array) $atts, CASE_LOWER );
-        
-            // override default attributes with user attributes
-            $wporg_atts = shortcode_atts(
-                array(
-                    'title' => 'WordPress.org',
-                ), $atts, $tag
-            );
-        
-            // start box
-            $o = '<div class="wporg-box">';
-        
-            // title
-            $o .= '<h2>' . esc_html__( $wporg_atts['title'], 'wporg' ) . '</h2>';
-        
-            // enclosing tags
-            if ( ! is_null( $content ) ) {
-                // $content here holds everything in between the opening and the closing tags of your shortcode. eg.g [my-shortcode]content[/my-shortcode].
-                // Depending on what your shortcode supports, you will parse and append the content to your output in different ways.
-                // In this example, we just secure output by executing the_content filter hook on $content.
-                $o .= apply_filters( 'the_content', $content );
-            }
-        
-            // end box
-            $o .= '</div>';
-        
-            // return output
-            return $o;
-        }
-        
 
-
-         function product_info( $atts = [] ) {
+        function product_info() {
 
             $curtain_user_id='';
 
@@ -141,20 +77,7 @@ if (!class_exists('otp_service')) {
 
                 unset($_POST['submit_action']);
             }
-/*
-            // normalize attribute keys, lowercase
-            $atts = array_change_key_case( (array) $atts, CASE_LOWER );
-        
-            // override default attributes with user attributes
-            $wporg_atts = shortcode_atts(
-                array(
-                    'title' => 'Curtains',
-                ), $atts
-            );
-            //$wporg_atts = shortcode_atts($atts);
-            $qr_code_id=$wporg_atts['id'];
-            $qr_code_action=$wporg_atts['action'];
-*/
+
             $output = '<div>';
             global $wpdb;
             $row = $wpdb->get_row( "SELECT * FROM {$wpdb->prefix}serial_number WHERE qr_code_id = {$qr_code_id}", OBJECT );
@@ -181,96 +104,46 @@ if (!class_exists('otp_service')) {
                 $output .= '請利用手機按 '.'<a href="https://line.me/ti/p/@490tjxdt">';
                 $output .= '<img src="https://scdn.line-apps.com/n/line_add_friends/btn/zh-Hant.png" alt="加入好友" height="36" border="0"></a>';
                 $output .= ' 加入我們的官方帳號, 讓我們成為您的好友,<br> 並在Line聊天室中重傳QR-code圖檔, 完成註冊程序<br>';
+
                 if( isset($_GET['id']) ) {
                     $output .= 'qr_code_id='.$_GET['id'].'<br>';
                 }
-            }
-            $output .= '</div>';
-            return $output;
-        }
 
-        function issue_otp() {
-
-            if( isset($_POST['submit_action']) ) {
-
-                if( $_POST['submit_action']=='Issue OTP' ) {
-                    $six_digit_random_number = random_int(100000, 999999);
-                    $client = line_bot_sdk();
-                    $client->pushMessage([
-                        //'to' => $line_user_id,
-                        'to' => 'U1b08294900a36077765643d8ae14a402',
-                        'messages' => [
-                            [
-                                'type' => 'text',
-                                'text' => 'OTP code : '.$six_digit_random_number
-                            ]
-                        ]
-                    ]);                
+                if( isset($_GET['action']) ) {
+                    if( ($_GET['action']=='insert_serial_number') && (isset($_GET['curtain_product_id'])) && (isset($_GET['curtain_user_id']))) {
+                        $data=array();
+                        $data['curtain_product_id']=$_GET['curtain_product_id']
+                        $data['curtain_user_id']=$_GET['curtain_user_id']
+                        self::insert_serial_number($data)
+                        $output .= 'Successed to insert a record in table serial_number<br>';
+                    }
                 }
-                unset($_POST['submit_action']);
             }
-
-            $output  = '<form method="post">';
-            $output .= '<div class="wp-block-button">';
-            $output .= '<input class="wp-block-button__link" type="submit" value="Issue OTP" name="submit_action">';
             $output .= '</div>';
-            $output .= '</form>';
             return $output;
-
         }
 
-        /**
-         * List Mode
-         */
-        function list_mode() {
+        function list_serial_number() {
             global $wpdb;
-            $results = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}eventLogs WHERE event_type = 'message' ORDER BY event_timestamp DESC LIMIT 10", OBJECT );
-            $output  = '<h2>Message Events</h2>';
+            $results = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}serial_number", OBJECT );
+            $output  = '<h2>Serial Number</h2>';
             $output .= '<figure class="wp-block-table"><table><tbody>';
-            //$output .= '<tr><td>Timestamp</td><td>EventObject</td><td>Source</td><td>UserId</td></tr>';
-            $output .= '<tr><td>User</td><td>EventObject</td><td>Source</td></tr>';
+            $output .= '<tr>';
+            $output .= '<td>qr_code_id</td>';
+            $output .= '<td>curtain_product</td>';
+            $output .= '<td>curtain_user</td>';
+            $output .= '<td>update_time</td>';
+            $output .= '</tr>';
             foreach ( $results as $index=>$result ) {
-                if ($result->source_type=='user'){
-                    $profile = self::line_bot_sdk()->getProfile($result->source_user_id);
-                    $group_name = $profile['displayName'];
-                    $group_picture_url = $profile['pictureUrl'];
-                    $display_name = $profile['displayName'];
-                    $user_picture_url = $profile['pictureUrl'];
-                } else {
-                    $summary = self::line_bot_sdk()->getGroupSummary($result->source_group_id);
-                    $group_name = $summary['groupName'];
-                    $group_picture_url = $summary['pictureUrl'];
-                    $profile = self::line_bot_sdk()->getGroupMemberProfile($result->source_group_id, $result->source_user_id);
-                    $display_name = $profile['displayName'];
-                    $user_picture_url = $profile['pictureUrl'];
-                }
-                $display_message = '';
-                $message = json_decode($result->event_object, true);
-                
-                switch ($message['type']) {
-                    case 'text':
-                        $display_message = $message['text'];
-                        break;
-                    case 'image':
-                        $content = self::line_bot_sdk()->getContent($message['id']);
-                        $display_message = $content;
-                        //$display_message = $message['id'];
-                        //$display_message = $message['contentProvider']['type'];
-                        break;
-                    default:
-                        $display_message = json_encode($message);
-                        break;
-                }
-                
                 $output .= '<tr>';
-                //$output .= '<td>'.$result->event_timestamp.'</td>';
-                $output .= '<td>'.'<img src="'.$user_picture_url.'" width="50" height="50" style="border-radius: 50%">'.$display_name.'</td>';
-                $output .= '<td>'.$display_message.'('.$message['type'].')'.'</td>';
-                $output .= '<td>'.'<img src="'.$group_picture_url.'" width="50" height="50" style="border-radius: 50%">'.$group_name.'('.$result->source_type.')'.'</td>';
+                $output .= '<td>'.$result->qr_code_id.'</td>';
+                $output .= '<td>'.$result->curtain_product_id.'</td>';
+                $output .= '<td>'.$result->curtain_user_id.'</td>';
+                $output .= '<td>'.$result->update_timestamp.'</td>';
                 $output .= '</tr>';
             }
             $output .= '</tbody></table></figure>';
-
+/*
             $output .= '<form method="get">';
             $output .= '<div class="wp-block-buttons">';
             $output .= '<div class="wp-block-button">';
@@ -282,144 +155,22 @@ if (!class_exists('otp_service')) {
             $output .= '</div>';
             $output .= '</div>';
             $output .= '</form>';
-
+*/
             return $output;
         }
 
-        public function insertEvent($event) {
-
-            switch ($event['type']) {
-                case 'message':
-                    $event_object = $event['message'];
-                    break;
-                case 'unsend':
-                    $event_object = $event['unsend'];
-                    break;
-                case 'memberJoined':
-                    $event_object = $event['joined'];
-                    break;
-                case 'memberLeft':
-                    $event_object = $event['left'];
-                    break;
-                case 'postback':
-                    $event_object = $event['postback'];
-                    break;
-                case 'videoPlayComplete':
-                    $event_object = $event['videoPlayComplete'];
-                    break;
-                case 'beacon':
-                    $event_object = $event['beacon'];
-                    break;
-                case 'accountLink':
-                    $event_object = $event['link'];
-                    break;
-                case 'things':
-                    $event_object = $event['things'];
-                    break;
-            }
-
-            switch ($event['source']['type']) {
-                case 'user':
-                    $source_type = $event['source']['type'];
-                    $user_id = $event['source']['userId'];
-                    $group_id = $event['source']['userId'];
-                    break;
-                case 'group':
-                    $source_type = $event['source']['type'];
-                    $user_id = $event['source']['userId'];
-                    $group_id = $event['source']['groupId'];
-                    break;
-                case 'room':
-                    $source_type = $event['source']['type'];
-                    $user_id = $event['source']['userId'];
-                    $group_id = $event['source']['roomId'];
-                    break;
-            }
+        function insert_serial_number($data=[]) {
 
             global $wpdb;
-            $table = $wpdb->prefix.'eventLogs';
+            $table = $wpdb->prefix.'serial_number';
             $data = array(
-                'event_type' => $event['type'],
-                'event_timestamp' => time(),
-                'source_type' => $source_type,
-                'source_user_id' => $user_id,
-                'source_group_id' => $group_id,
-                'event_replyToken' => $event['replyToken'],
-                'event_mode' => $event['mode'],
-                'webhookEventId' => $event['webhookEventId'],
-                'isRedelivery' => $event['deliveryContext']['isRedelivery'],
-                'event_object' => json_encode($event_object),
+                'qr_code_id' => time(),
+                'curtain_product_id' => $data['curtain_product_id'],
+                'curtain_user_id' => $data['curtain_user_id'],
+                'create_timestamp' => time(),
+                'update_timestamp' => time(),
             );
-            $insert_id = $wpdb->insert($table, $data);        
-        }
-    
-        public function insertMessageEvent($event) {
-
-            $message = $event['message'];
-
-            switch ($event['source']['type']) {
-                case 'user':
-                    $source_type = $event['source']['type'];
-                    $user_id = $event['source']['userId'];
-                    $group_id = $event['source']['userId'];
-                    break;
-                case 'group':
-                    $source_type = $event['source']['type'];
-                    $user_id = $event['source']['userId'];
-                    $group_id = $event['source']['groupId'];
-                    break;
-                case 'room':
-                    $source_type = $event['source']['type'];
-                    $user_id = $event['source']['userId'];
-                    $group_id = $event['source']['roomId'];
-                    break;
-            }
-
-            global $wpdb;
-            $table = $wpdb->prefix.'messageEvents';
-            $data = array(
-                'event_timestamp' => time(),
-                'message_type' => $event['message']['type'],
-                'source_type' => $source_type,
-                'source_user_id' => $user_id,
-                'source_group_id' => $group_id,
-                'webhookEventId' => $event['webhookEventId'],
-                'event_message' => json_encode($message),
-            );
-            $insert_id = $wpdb->insert($table, $data);        
-        }
-
-        public function insertTextMessage($event) {
-
-            switch ($event['source']['type']) {
-                case 'user':
-                    $source_type = $event['source']['type'];
-                    $user_id = $event['source']['userId'];
-                    $group_id = $event['source']['userId'];
-                    break;
-                case 'group':
-                    $source_type = $event['source']['type'];
-                    $user_id = $event['source']['userId'];
-                    $group_id = $event['source']['groupId'];
-                    break;
-                case 'room':
-                    $source_type = $event['source']['type'];
-                    $user_id = $event['source']['userId'];
-                    $group_id = $event['source']['roomId'];
-                    break;
-            }
-
-            global $wpdb;
-            $table = $wpdb->prefix.'textMessages';
-            $data = array(
-                'event_timestamp' => time(),
-                'source_type' => $source_type,
-                'source_user_id' => $user_id,
-                'source_group_id' => $group_id,
-                'webhookEventId' => $event['webhookEventId'],
-                'textMessage_text' => $event['message']['text'],
-            );
-            $insert_id = $wpdb->insert($table, $data);        
+            $wpdb->insert($table, $data);        
         }
 
         function delete_records() {
