3a008186f2212796d656a5385eae966aaece9d3e
Rover Chen <rover.k.chen@gmail.com>
2022-09-12 15:08:31 +0800

    $client = $event_bot->line_bot_sdk();

--- line-event-bot.php
+++ line-event-bot.php
@@ -22,9 +22,10 @@ include_once dirname( __FILE__ ) . '/line-bot-sdk-tiny/LINEBotTiny.php';
 include_once dirname( __FILE__ ) . '/includes/class-event-bot.php';
 include_once dirname( __FILE__ ) . '/includes/class-otp-service.php';
 
-/*
-$event_bot = new event_bot();
-$client = $event_bot->line_bot_sdk();
+
+//$event_bot = new event_bot();
+//$client = $event_bot->line_bot_sdk();
+$client = line_bot_sdk();
 
 foreach ($client->parseEvents() as $event) {
     $event_bot->insertEvent($event);
@@ -65,7 +66,7 @@ foreach ($client->parseEvents() as $event) {
             break;
     }    
 };
-*/
+
 
 function line_bot_sdk() {
     $channelAccessToken = '';
