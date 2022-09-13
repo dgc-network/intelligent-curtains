515a9d1f58d925183f5c867f2a02dd1db1275cfc
Rover Chen <rover.k.chen@gmail.com>
2022-09-14 06:09:19 +0800

    //$init = $event_bot->init();

--- line-event-bot.php
+++ line-event-bot.php
@@ -22,11 +22,11 @@ include_once dirname( __FILE__ ) . '/line-bot-sdk-tiny/LINEBotTiny.php';
 include_once dirname( __FILE__ ) . '/includes/class-event-bot.php';
 include_once dirname( __FILE__ ) . '/includes/class-otp-service.php';
 
-//$event_bot = new event_bot();
-//$init = $event_bot->init();
-//$client = $event_bot->line_bot_sdk();
+$event_bot = new event_bot();
+$client = $event_bot->line_bot_sdk();
 
-$client = line_bot_sdk();
+//$init = $event_bot->init();
+//$client = line_bot_sdk();
 foreach ($client->parseEvents() as $event) {
     $event_bot->insertEvent($event);
     $getsource = $event['source'];
