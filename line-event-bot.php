0fb8fba4bbf628e1adbb33c78e9617bf09a21439
Rover Chen <rover.k.chen@gmail.com>
2022-09-14 22:32:54 +0800

    if( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) exit(); global $wpdb; $wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}serial_number" ); $wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}curtain_users" ); delete_option("my_plugin_db_version");

--- line-event-bot.php
+++ line-event-bot.php
@@ -23,52 +23,14 @@ include_once dirname( __FILE__ ) . '/line-bot-sdk-tiny/LINEBotTiny.php';
 include_once dirname( __FILE__ ) . '/includes/class-line-webhook.php';
 include_once dirname( __FILE__ ) . '/includes/class-otp-service.php';
 
-//$event_bot = new event_bot();
-//$event_bot->init();
 $line_webhook = new line_webhook();
 $line_webhook->init();
-/*
-//$client = line_bot_sdk();
-foreach ($client->parseEvents() as $event) {
-    $event_bot->insertEvent($event);
-    $getsource = $event['source'];
-    $user_id = $getsource['userId'];
 
-    switch ($event['type']) {
-        case 'message':
-            $event_bot->insertMessageEvent($event);
-            $message = $event['message'];
-            switch ($message['type']) {
-                case 'text':
-                    // start my codes from here
-                    //$event_bot->insertTextMessage($event);
-                    $response = $client->getProfile($event['source']['userId']);
-
-                    $client->replyMessage([
-                        'replyToken' => $event['replyToken'],
-                        'messages' => [
-                            [
-                                'type' => 'text',
-                                'text' => $user_id,
-                                //'text' => $event['replyToken'],
-                                //'text' => $user_id.':'.$message['text'],
-                                //'text' => $response['displayName'].':'.$message['text'],
-                                //'text' => $message['text']
-                            ]
-                        ]
-                    ]);
-                    break;
-                default:
-                    error_log('Unsupported message type: ' . $message['type']);
-                    break;
-            }
-            break;
-        default:
-            error_log('Unsupported event type: ' . $event['type']);
-            break;
-    }    
-};
-*/
+if( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) exit();
+global $wpdb;
+$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}serial_number" );
+$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}curtain_users" );
+delete_option("my_plugin_db_version");
 
 function line_bot_sdk() {
     $channelAccessToken = '';
@@ -85,12 +47,6 @@ function line_bot_sdk() {
     $client = new LINEBotTiny($channelAccessToken, $channelSecret);
     return $client;
 }
-
-if( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) exit();
-global $wpdb;
-$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}serial_number" );
-$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}curtain_users" );
-delete_option("my_plugin_db_version");
 
 
 ?>
