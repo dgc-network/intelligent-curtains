0a30893801d6f30242f04da13397321d111b4f14
Rover Chen <rover.k.chen@gmail.com>
2022-09-16 18:15:34 +0800

    <p>This is the default dialog which is useful for displaying information. The dialog window can be moved, resized and closed with the 'x' icon.</p> </div>

--- includes/class-otp-service.php
+++ includes/class-otp-service.php
@@ -52,7 +52,11 @@ if (!class_exists('otp_service')) {
                     $serial_no = $_POST['serial_no'];
                     global $wp;
                     //echo home_url( $wp->request );
-                    return do_shortcode( '[dqr_code url="'.home_url( $wp->request ).'?serial_no='.$serial_no.'"]' );
+                    $output = '<div id="dialog" title="Basic dialog">';
+                    $output .= '<p>This is the default dialog which is useful for displaying information. The dialog window can be moved, resized and closed with the icon.</p></div>';
+                    return $output;
+
+                    //return do_shortcode( '[dqr_code url="'.home_url( $wp->request ).'?serial_no='.$serial_no.'"]' );
                 }
 
                 if( $_POST['submit_action']=='Confirm' ) {
@@ -420,9 +424,6 @@ if (!class_exists('otp_service')) {
         $( "#dialog" ).dialog();
     });
 </script>
-<div id="dialog" title="Basic dialog">
-    <p>This is the default dialog which is useful for displaying information. The dialog window can be moved, resized and closed with the 'x' icon.</p>
-</div>
 
 <script>
 /*    
