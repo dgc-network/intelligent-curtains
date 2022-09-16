66f9821a990fed4d98db92d186ef0f276c935553
Rover Chen <rover.k.chen@gmail.com>
2022-09-16 21:30:42 +0800

    ?> <script> /*     $(document).ready(function() {         $( "#dialog" ).dialog();     });
        jQuery(function() {
            $( "#dialog" ).dialog();
        });
    */
    </script>
    
    <?php

--- includes/class-otp-service.php
+++ includes/class-otp-service.php
@@ -52,21 +52,6 @@ if (!class_exists('otp_service')) {
                     $serial_no = $_POST['serial_no'];
                     global $wp;
                     //echo home_url( $wp->request );
-?>
-<script>
-/*
-    $(document).ready(function() {
-        $( "#dialog" ).dialog();
-    });
-    
-    jQuery(function() {
-        $( "#dialog" ).dialog();
-    });
-*/    
-</script>
-
-
-<?php
 
                     $output = '<div id="dialog" title="Basic dialog">';
                     $output .= '<p>This is the default dialog which is useful for displaying information. The dialog window can be moved, resized and closed with the icon.</p></div>';
