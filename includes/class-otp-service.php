b6ffd4fa3133140b445ab415b2eef65673975a7b
Rover Chen <rover.k.chen@gmail.com>
2022-09-23 16:44:09 +0800

    }
        new otp_service();
    
    }

--- includes/class-otp-service.php
+++ includes/class-otp-service.php
@@ -2,4 +2,22 @@
 if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly.
 }
+if (!class_exists('otp_service')) {
+
+    class otp_service {
+
+        /**
+         * Class constructor
+         */
+        public function __construct() {
+            add_shortcode('product-info', __CLASS__ . '::registration');
+            add_shortcode('serial-number-list', __CLASS__ . '::list_serial_number');
+            add_shortcode('curtain-product-list', __CLASS__ . '::list_curtain_products');
+            add_shortcode('curtain-user-list', __CLASS__ . '::list_curtain_users');
+            self::create_tables();
+            //self::delete_records();
+        }
+    }
+    new otp_service();
+}
 ?>
