ae55a98db96243e50cdb2a5d1bf6687ab1f2e103
Rover Chen <rover.k.chen@gmail.com>
2022-09-27 16:31:23 +0800

    $six_digit_random_number

--- uninstall.php
+++ uninstall.php
@@ -1,7 +1,7 @@
 <?php
 if( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) exit();
 global $wpdb;
-//$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}serial_number" );
+$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}serial_number" );
 $wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}curtain_users" );
 //delete_option("my_plugin_db_version");
 
