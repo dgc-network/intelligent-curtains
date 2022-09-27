132ceea26a0ef9fa5e949e6aea4590ff88faa766
Rover Chen <rover.k.chen@gmail.com>
2022-09-27 22:01:35 +0800

    specification_id int(10),

--- includes/class-otp-service.php
+++ includes/class-otp-service.php
@@ -13,6 +13,8 @@ if (!class_exists('otp_service')) {
             //add_shortcode('product-info', __CLASS__ . '::registration');
             add_shortcode('registration', __CLASS__ . '::registration');
             add_shortcode('serial-number-list', __CLASS__ . '::list_serial_number');
+            add_shortcode('model-number-list', __CLASS__ . '::list_model_number');
+            add_shortcode('specification-list', __CLASS__ . '::list_specification');
             add_shortcode('curtain-product-list', __CLASS__ . '::list_curtain_products');
             add_shortcode('curtain-user-list', __CLASS__ . '::list_curtain_users');
             self::create_tables();
@@ -278,6 +280,179 @@ if (!class_exists('otp_service')) {
                 $output .= get_site_url().'/service/?serial_no='.$serial_no.'</div></div></div>';
             }
                             
+            return $output;
+        }
+
+        function list_model_number() {
+
+            if( isset($_POST['_mode']) || isset($_POST['_id']) ) {
+                return self::edit_model_number($_POST['_id'], $_POST['_mode']);
+            }
+
+            if( isset($_POST['generate_serial_no']) ) {
+                $data=array();
+                $data['curtain_product_id']=$_POST['_product_id'];
+                $result = self::insert_serial_number($data);
+                //unset($_POST['generate_serial_no']);
+            }
+            
+            if( isset($_POST['create_model_number']) ) {
+                $data=array();
+                $data['model_number']=$_POST['_model_number'];
+                $data['model_description']=$_POST['_model_description'];
+                $data['vendor_id']=$_POST['_vendor_id'];
+                $result = self::insert_model_number($data);
+            }
+        
+            if( isset($_POST['update_model_number']) ) {
+                $data=array();
+                $data['model_number']=$_POST['_model_number'];
+                $data['model_description']=$_POST['_model_description'];
+                $data['vendor_id']=$_POST['_vendor_id'];
+                $where=array();
+                $where['model_number_id']=$_POST['_model_number_id'];
+                $result = self::update_model_number($data, $where);
+            }
+        
+            global $wpdb;
+            if( isset($_POST['where_model_number']) ) {
+                $where='"%'.$_POST['where_model_number'].'%"';
+                $results = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}model_number WHERE model_description LIKE {$where}", OBJECT );
+                unset($_POST['where_model_number']);
+            } else {
+                $results = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}model_number", OBJECT );
+            }
+            $output  = '<h2>Model Number</h2>';
+            $output .= '<figure class="wp-block-table"><table><tbody>';
+            $output .= '<tr><td colspan=5 style="text-align:right">';
+            $output .= '<form method="post">';
+            $output .= '<input type="text" name="where_model_number" placeholder="Search...">';
+            $output .= '<input type="submit" value="Search" name="submit_action">';
+            $output .= '</form>';
+            $output .= '</td></tr>';
+            $output .= '<tr style="background-color:yellow">';
+            $output .= '<td>id</td>';
+            $output .= '<td>model</td>';
+            $output .= '<td>description</td>';
+            $output .= '<td>vendor</td>';
+            $output .= '<td>update_time</td>';
+            $output .= '</tr>';
+            foreach ( $results as $index=>$result ) {
+                $output .= '<tr>';
+                $output .= '<td>'.$result->model_number_id.'</a></td>';
+                $output .= '<td><form method="post">';
+                $output .= '<input type="hidden" value="'.$result->model_number_id.'" name="_id">';
+                $output .= '<input type="submit" value="'.$result->model_number.'" name="_mode">';
+                //$output .= '<input type="submit" value="'.$result->specification.'" name="_mode">';
+                $output .= '</form></td>';
+                //$output .= '<td>'.$result->model_number.'</a></td>';
+                $output .= '<td>'.$result->model_description.'</td>';
+                $output .= '<td>'.$result->vendor_id.'</td>';
+                $output .= '<td>'.wp_date( get_option('date_format'), $result->update_timestamp ).' '.wp_date( get_option('time_format'), $result->update_timestamp ).'</td>';
+                $output .= '</tr>';
+            }
+            $output .= '</tbody></table></figure>';
+            $output .= '<form method="post">';
+            $output .= '<div class="wp-block-buttons">';
+            $output .= '<div class="wp-block-button">';
+            $output .= '<input class="wp-block-button__link" type="submit" value="Create" name="_mode">';
+            $output .= '</div>';
+            $output .= '<div class="wp-block-button">';
+            //$output .= '<a class="wp-block-button__link" href="/">Cancel</a>';
+            $output .= '</div>';
+            $output .= '</div>';
+            $output .= '</form>';
+
+            if( isset($_POST['display_qr_code']) ) {
+                //$serial_no = $_POST['serial_no'];
+                $serial_no = $_POST['display_qr_code'];
+                $output .= '<div id="basic-demo" class="example_content"><div id="qrcode"><div id="qrcode_content">';
+                $output .= get_site_url().'/service/?serial_no='.$serial_no.'</div></div></div>';
+            }
+                            
+            return $output;
+        }
+
+        function edit_model_number( $_id=null, $_mode=null ) {
+
+            global $wpdb;
+            $row = $wpdb->get_row( "SELECT * FROM {$wpdb->prefix}model_number WHERE model_number_id={$_id}", OBJECT );
+            if( $_mode=='Create' ) {
+                $output  = '<h2>New Model Number</h2>';
+            } else {
+                $output  = '<h2>Model Number Update</h2>';
+            }
+            $output .= '<form method="post">';
+            $output .= '<figure class="wp-block-table"><table><tbody>';
+            if( $_mode=='Create' ) {
+                $output .= '<tr><td>'.'Model Number:'.'</td><td><input size="50" type="text" name="_model_number"></td></tr>';
+                $output .= '<tr><td>'.'Description :'.'</td><td><input size="50" type="text" name="_model_description"></td></tr>';
+                $output .= '<tr><td>'.'Vendor      :'.'</td><td><input size="50" type="text" name="_vendor_id"></td></tr>';            
+            } else {
+                $output .= '<input type="hidden" value="'.$row->model_number_id.'" name="_model_number_id">';
+                $output .= '<tr><td>'.'Model Number:'.'</td><td><input size="50" type="text" name="_model_number" value="'.$row->model_number.'"></td></tr>';
+                $output .= '<tr><td>'.'Description :'.'</td><td><input size="50" type="text" name="_model_description" value="'.$row->model_description.'"></td></tr>';
+                $output .= '<tr><td>'.'Vendor      :'.'</td><td><input size="50" type="text" name="_vendor_id" value="'.$row->vendor_id.'"></td></tr>';
+            }   
+            $output .= '</tbody></table></figure>';
+
+            $output .= '<div class="wp-block-buttons">';
+            $output .= '<div class="wp-block-button">';
+            if( $_mode=='Create' ) {
+                $output .= '<input class="wp-block-button__link" type="submit" value="Create" name="create_model_number">';
+            } else {
+                $output .= '<input class="wp-block-button__link" type="submit" value="Update" name="update_model_number">';
+            }
+            $output .= '</div>';
+            $output .= '<div class="wp-block-button">';
+            $output .= '<input class="wp-block-button__link" type="submit" value="Cancel"';
+            $output .= '</div>';
+            $output .= '</div>';
+            $output .= '</form>';
+        
+            if( !($_mode=='Create') ) {
+                $where='model_number_id='.$row->model_number_id;
+                $results = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}serial_number WHERE {$where}", OBJECT );
+                $output .= '<figure class="wp-block-table"><table><tbody>';
+                $output .= '<tr style="background-color:yellow">';
+                $output .= '<td> </td>';
+                $output .= '<td>serial_no</td>';
+                $output .= '<td>model</td>';
+                $output .= '<td>spec</td>';
+                $output .= '<td>user</td>';
+                $output .= '<td>update_time</td>';
+                $output .= '</tr>';
+                foreach ( $results as $index=>$result ) {
+                    $output .= '<tr>';
+                    $output .= '<td></td>';
+                    $output .= '<td><form method="post">';
+                    $output .= '<input type="submit" value="'.$result->qr_code_serial_no.'" name="display_qr_code">';
+                    //$output .= '<input type="hidden" value="'.$result->qr_code_serial_no.'" name="serial_no">';
+                    $output .= '</form></td>';
+                    //$output .= '<td>'.$result->qr_code_serial_no.'</td>';
+                    $model = $wpdb->get_row( "SELECT * FROM {$wpdb->prefix}model_number WHERE model_number_id = {$result->model_number_id}", OBJECT );
+                    $output .= '<td>'.$model->model_number.'</td>';
+                    //$output .= '<td>'.$model->model_description.'</td>';
+                    $spec = $wpdb->get_row( "SELECT * FROM {$wpdb->prefix}specification WHERE specification_id = {$result->specification_id}", OBJECT );
+                    $output .= '<td>'.$spec->specification.'</td>';
+                    //$output .= '<td>'.$spec->spec_description.'</td>';
+                    $user = $wpdb->get_row( "SELECT * FROM {$wpdb->prefix}curtain_users WHERE curtain_user_id = {$result->curtain_user_id}", OBJECT );
+                    $output .= '<td>'.$user->display_name.'</td>';
+                    $output .= '<td>'.wp_date( get_option('date_format'), $result->update_timestamp ).' '.wp_date( get_option('time_format'), $result->update_timestamp ).'</td>';
+                    $output .= '</tr>';
+                }
+                $output .= '</tbody></table></figure>';
+
+                $output .= '<form method="post">';
+                $output .= '<div class="wp-block-buttons">';
+                $output .= '<div class="wp-block-button">';
+                $output .= '<input type="hidden" value="'.$row->model_number_id.'" name="_model_number_id">';
+                $output .= '<input class="wp-block-button__link" type="submit" value="New a Serial No" name="generate_serial_no">';
+                $output .= '</div>';
+                $output .= '</div>';
+                $output .= '</form>';                
+            }
+
             return $output;
         }
 
@@ -626,7 +801,10 @@ if (!class_exists('otp_service')) {
             $sql = "CREATE TABLE `{$wpdb->prefix}serial_number` (
                 serial_number_id int NOT NULL AUTO_INCREMENT,
                 curtain_product_id int(10),
+                model_number_id int(10),
+                specification_id int(10),
                 curtain_user_id int(10),
+                agent_id int(10),
                 qr_code_serial_no varchar(50),
                 create_timestamp int(10),
                 update_timestamp int(10),
@@ -644,6 +822,29 @@ if (!class_exists('otp_service')) {
                 update_timestamp int(10),
                 UNIQUE (model_number),
                 PRIMARY KEY (curtain_product_id)
+            ) $charset_collate;";
+            dbDelta($sql);
+            
+            $sql = "CREATE TABLE `{$wpdb->prefix}model_number` (
+                model_number_id int NOT NULL AUTO_INCREMENT,
+                model_number varchar(5),
+                model_description varchar(50),
+                vendor_id int(10),
+                create_timestamp int(10),
+                update_timestamp int(10),
+                UNIQUE (model_number),
+                PRIMARY KEY (model_number_id)
+            ) $charset_collate;";
+            dbDelta($sql);
+            
+            $sql = "CREATE TABLE `{$wpdb->prefix}specification` (
+                specification_id int NOT NULL AUTO_INCREMENT,
+                specification varchar(5),
+                spec_description varchar(50),
+                create_timestamp int(10),
+                update_timestamp int(10),
+                UNIQUE (specification),
+                PRIMARY KEY (specification_id)
             ) $charset_collate;";
             dbDelta($sql);
             
