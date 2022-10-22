b65f271500c11f24644a69bfbb236eda0c0ed84c
Rover Chen <rover.k.chen@gmail.com>
2022-10-22 19:03:13 +0800

    /* jQuery UI Dialog - Modal form */

--- includes/class-curtain-models.php
+++ includes/class-curtain-models.php
@@ -23,94 +23,6 @@ if (!class_exists('curtain_models')) {
             <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
             <script>
             $( function() {
-              var dialog, form,
-           
-                // From http://www.whatwg.org/specs/web-apps/current-work/multipage/states-of-the-type-attribute.html#e-mail-state-%28type=email%29
-                emailRegex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
-                name = $( "#name" ),
-                email = $( "#email" ),
-                password = $( "#password" ),
-                allFields = $( [] ).add( name ).add( email ).add( password ),
-                tips = $( ".validateTips" );
-           
-              function updateTips( t ) {
-                tips
-                  .text( t )
-                  .addClass( "ui-state-highlight" );
-                setTimeout(function() {
-                  tips.removeClass( "ui-state-highlight", 1500 );
-                }, 500 );
-              }
-           
-              function checkLength( o, n, min, max ) {
-                if ( o.val().length > max || o.val().length < min ) {
-                  o.addClass( "ui-state-error" );
-                  updateTips( "Length of " + n + " must be between " +
-                    min + " and " + max + "." );
-                  return false;
-                } else {
-                  return true;
-                }
-              }
-           
-              function checkRegexp( o, regexp, n ) {
-                if ( !( regexp.test( o.val() ) ) ) {
-                  o.addClass( "ui-state-error" );
-                  updateTips( n );
-                  return false;
-                } else {
-                  return true;
-                }
-              }
-           
-              function addUser() {
-                var valid = true;
-                allFields.removeClass( "ui-state-error" );
-           
-                valid = valid && checkLength( name, "username", 3, 16 );
-                valid = valid && checkLength( email, "email", 6, 80 );
-                valid = valid && checkLength( password, "password", 5, 16 );
-           
-                valid = valid && checkRegexp( name, /^[a-z]([0-9a-z_\s])+$/i, "Username may consist of a-z, 0-9, underscores, spaces and must begin with a letter." );
-                valid = valid && checkRegexp( email, emailRegex, "eg. ui@jquery.com" );
-                valid = valid && checkRegexp( password, /^([0-9a-zA-Z])+$/, "Password field only allow : a-z 0-9" );
-           
-                if ( valid ) {
-                  $( "#users tbody" ).append( "<tr>" +
-                    "<td>" + name.val() + "</td>" +
-                    "<td>" + email.val() + "</td>" +
-                    "<td>" + password.val() + "</td>" +
-                  "</tr>" );
-                  dialog.dialog( "close" );
-                }
-                return valid;
-              }
-           
-              dialog = $( "#dialog-form" ).dialog({
-                autoOpen: false,
-                height: 400,
-                width: 350,
-                modal: true,
-                buttons: {
-                  "Create an account": addUser,
-                  Cancel: function() {
-                    dialog.dialog( "close" );
-                  }
-                },
-                close: function() {
-                  form[ 0 ].reset();
-                  allFields.removeClass( "ui-state-error" );
-                }
-              });
-           
-              form = dialog.find( "form" ).on( "submit", function( event ) {
-                event.preventDefault();
-                addUser();
-              });
-           
-              $( "#create-user" ).button().on( "click", function() {
-                dialog.dialog( "open" );
-              });
             } );
             </script>
 
@@ -132,28 +44,6 @@ if (!class_exists('curtain_models')) {
               </fieldset>
             </form>
           </div>
-           
-           
-          <div id="users-contain" class="ui-widget">
-            <h1>Existing Users:</h1>
-            <table id="users" class="ui-widget ui-widget-content">
-              <thead>
-                <tr class="ui-widget-header ">
-                  <th>Name</th>
-                  <th>Email</th>
-                  <th>Password</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>John Doe</td>
-                  <td>john.doe@example.com</td>
-                  <td>johndoe1</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-          <button id="create-user">Create new user</button>
 <?php          
           if( isset($_POST['_mode']) || isset($_POST['_id']) ) {
                 return self::edit_curtain_model($_POST['_id'], $_POST['_mode']);
@@ -211,6 +101,31 @@ if (!class_exists('curtain_models')) {
             } else {
                 $results = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}curtain_models", OBJECT );
             }
+?>
+           
+           
+           <div id="users-contain" class="ui-widget">
+            <h1>Existing Users:</h1>
+            <table id="users" class="ui-widget ui-widget-content">
+              <thead>
+                <tr class="ui-widget-header ">
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Password</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>John Doe</td>
+                  <td>john.doe@example.com</td>
+                  <td>johndoe1</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <button id="create-user">Create new user</button>
+
+<?php            
             $output  = '<h2>Model Number</h2>';
             $output .= '<figure class="wp-block-table"><table><tbody>';
             $output .= '<tr><td colspan=5 style="text-align:right">';
