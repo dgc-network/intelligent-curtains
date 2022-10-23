05f2d95b3d4269a778618b7fb5c8d40cc41128fc
Rover Chen <rover.k.chen@gmail.com>
2022-10-23 21:10:45 +0800

    function enqueue_scripts() {
    		//wp_enqueue_script( 'jquery-js', MY_PLUGIN_DIR . 'assets/js/jquery.min.js', array( 'jquery' ), time(), true );
    
    		wp_enqueue_script( 'custom-js', plugin_dir_url( __DIR__ ) . 'assets/js/custom-options-view.js', array( 'jquery' ), time(), true );

--- includes/class-curtain-models.php
+++ includes/class-curtain-models.php
@@ -11,27 +11,17 @@ if (!class_exists('curtain_models')) {
          */
         public function __construct() {
             add_shortcode('curtain-model-list', __CLASS__ . '::list_curtain_models');
+            add_action( 'wp_enqueue_scripts', array( __CLASS__, 'enqueue_scripts' ) );
             self::create_tables();
         }
+
+        function enqueue_scripts() {		
+            wp_enqueue_script( 'custom-js', plugin_dir_url( __DIR__ ) . 'assets/js/custom-curtain-models.js', array( 'jquery' ), time(), true );
+        }    
 
         function list_curtain_models() {
 ?>
 
-            <style>
-
-/*            
-            label, input { display:block; }
-            input.text { margin-bottom:12px; width:95%; padding: .4em; }
-            fieldset { padding:0; border:0; margin-top:25px; }
-            h1 { font-size: 1.2em; margin: .6em 0; }
-            div#users-contain { width: 350px; margin: 20px 0; }
-            div#users-contain table { margin: 1em 0; border-collapse: collapse; width: 100%; }
-            div#users-contain table td, 
-            div#users-contain table th { border: 1px solid #eee; padding: .6em 10px; text-align: left; }
-            .ui-dialog .ui-state-error { padding: .3em; }
-            .validateTips { border: 1px solid transparent; padding: 0.3em; }
-*/            
-            </style>
             <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
             <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
 
