11da6f88eb5c4e10309e6202e9eac81b12a9caa0
Rover Chen <rover.k.chen@gmail.com>
2022-10-23 20:55:32 +0800

    ?>
                <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
                <link rel="stylesheet" href="/resources/demos/style.css">
                <style>
    
    /*
                label, input { display:block; }
                input.text { margin-bottom:12px; width:95%; padding: .4em; }
                fieldset { padding:0; border:0; margin-top:25px; }
                h1 { font-size: 1.2em; margin: .6em 0; }
                div#users-contain { width: 350px; margin: 20px 0; }
                div#users-contain table { margin: 1em 0; border-collapse: collapse; width: 100%; }
                div#users-contain table td,
                div#users-contain table th { border: 1px solid #eee; padding: .6em 10px; text-align: left; }
                .ui-dialog .ui-state-error { padding: .3em; }
                .validateTips { border: 1px solid transparent; padding: 0.3em; }
    */
                </style>
                <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
                <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
    
    <?php

---
+++ assets/js/custom-curtain-model.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function($) {
+
+    $('#qrcode').qrcode({
+        text: $("#qrcode_content").text()
+    });
+
+/* jQuery UI Dialog - Basic dialog */
+
+    $( "#dialog" ).dialog();
+
+/* jQuery UI Dialog - Modal form */
+
+    // From http://www.whatwg.org/specs/web-apps/current-work/multipage/states-of-the-type-attribute.html#e-mail-state-%28type=email%29
+
+});
