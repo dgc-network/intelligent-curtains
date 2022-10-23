32ea4c50cf808bb07e0da3efda3c900966fb4b13
Rover Chen <rover.k.chen@gmail.com>
2022-10-23 21:24:45 +0800

    wp_enqueue_script( 'jquery-ui-dialog' );

--- includes/class-curtain-models.php
+++ includes/class-curtain-models.php
@@ -17,6 +17,7 @@ if (!class_exists('curtain_models')) {
 
         function enqueue_scripts() {		
             wp_enqueue_script( 'custom-curtain-models', plugin_dir_url( __DIR__ ) . 'assets/js/custom-curtain-models.js', array( 'jquery' ), time(), true );
+            wp_enqueue_script( 'jquery-ui-dialog' );
             wp_enqueue_style( 'jquery-ui' );  
             wp_enqueue_style( 'demos-style' );  
             }    
