a4623bdeb181514c5a8ae0be0789f9d910bca153
Rover Chen <rover.k.chen@gmail.com>
2022-10-23 10:43:38 +0800

    function addUser() {                     var valid = true;                     allFields.removeClass( "ui-state-error" );
                        valid = valid && checkLength( name, "username", 3, 16 );
                        valid = valid && checkLength( email, "email", 6, 80 );
                        valid = valid && checkLength( password, "password", 5, 16 );
    
                        valid = valid && checkRegexp( name, /^[a-z]([0-9a-z_\s])+$/i, "Username may consist of a-z, 0-9, underscores, spaces and must begin with a letter." );
                        valid = valid && checkRegexp( email, emailRegex, "eg. ui@jquery.com" );
                        valid = valid && checkRegexp( password, /^([0-9a-zA-Z])+$/, "Password field only allow : a-z 0-9" );
    
                        if ( valid ) {
                            $( "#users tbody" ).append( "<tr>" +
                                "<td>" + name.val() + "</td>" +
                                "<td>" + email.val() + "</td>" +
                                "<td>" + password.val() + "</td>" +
                            "</tr>" );
                            dialog.dialog( "close" );
                        }
                        return valid;
                    }

--- includes/class-curtain-models.php
+++ includes/class-curtain-models.php
@@ -36,10 +36,6 @@ if (!class_exists('curtain_models')) {
             </style>
             <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
             <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
-            <script>
-            $( function() {
-            } );
-            </script>
 
 
 <?php          
@@ -95,25 +91,97 @@ if (!class_exists('curtain_models')) {
         
 
 ?>
-            <div id="dialog-form" title="Create new user">
-            <p class="validateTips">All form fields are required.</p>
-           
-            <form>
-              <fieldset>
-                <label for="name">Name</label>
-                <input type="text" name="name" id="name" value="Jane Smith" class="text ui-widget-content ui-corner-all">
-                <label for="email">Email</label>
-                <input type="text" name="email" id="email" value="jane@smith.com" class="text ui-widget-content ui-corner-all">
-                <label for="password">Password</label>
-                <input type="password" name="password" id="password" value="xxxxxxx" class="text ui-widget-content ui-corner-all">
-           
-                <!-- Allow form submission with keyboard without duplicating the dialog button -->
-                <input type="submit" tabindex="-1" style="position:absolute; top:-1000px">
-              </fieldset>
-            </form>
-          </div>
+            <script>
+            $( function() {
 
-
+                var dialog, form,           
+                emailRegex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
+                name = $( "#name" ),
+                email = $( "#email" ),
+                password = $( "#password" ),
+                allFields = $( [] ).add( name ).add( email ).add( password ),
+                tips = $( ".validateTips" );
+    
+                dialog = $( "#dialog-form" ).dialog({
+                    autoOpen: false,
+                    height: 400,
+                    width: 350,
+                    modal: true,
+                    buttons: {
+                        "Create": addUser,
+                        Cancel: function() {
+                            dialog.dialog( "close" );
+                        }
+                    },
+                    close: function() {
+                        form[ 0 ].reset();
+                        allFields.removeClass( "ui-state-error" );
+                    }
+                });
+    
+                form = dialog.find( "form" ).on( "submit", function( event ) {
+                    event.preventDefault();
+                    addUser();
+                });
+    
+                function updateTips( t ) {
+                    tips
+                        .text( t )
+                        .addClass( "ui-state-highlight" );
+                    setTimeout(function() {
+                        tips.removeClass( "ui-state-highlight", 1500 );
+                    }, 500 );
+                }
+    
+                function checkLength( o, n, min, max ) {
+                    if ( o.val().length > max || o.val().length < min ) {
+                        o.addClass( "ui-state-error" );
+                        updateTips( "Length of " + n + " must be between " +
+                            min + " and " + max + "." );
+                        return false;
+                    } else {
+                        return true;
+                    }
+                }
+    
+                function checkRegexp( o, regexp, n ) {
+                    if ( !( regexp.test( o.val() ) ) ) {
+                        o.addClass( "ui-state-error" );
+                        updateTips( n );
+                        return false;
+                    } else {
+                        return true;
+                    }
+                }
+    
+                $( "#create-button" ).button().on( "click", function() {
+                    dialog.dialog( "open" );
+                });
+                  
+                function addUser() {
+                    var valid = true;
+                    allFields.removeClass( "ui-state-error" );
+    
+                    valid = valid && checkLength( name, "username", 3, 16 );
+                    valid = valid && checkLength( email, "email", 6, 80 );
+                    valid = valid && checkLength( password, "password", 5, 16 );
+    
+                    valid = valid && checkRegexp( name, /^[a-z]([0-9a-z_\s])+$/i, "Username may consist of a-z, 0-9, underscores, spaces and must begin with a letter." );
+                    valid = valid && checkRegexp( email, emailRegex, "eg. ui@jquery.com" );
+                    valid = valid && checkRegexp( password, /^([0-9a-zA-Z])+$/, "Password field only allow : a-z 0-9" );
+    
+                    if ( valid ) {
+                        $( "#users tbody" ).append( "<tr>" +
+                            "<td>" + name.val() + "</td>" +
+                            "<td>" + email.val() + "</td>" +
+                            "<td>" + password.val() + "</td>" +
+                        "</tr>" );
+                        dialog.dialog( "close" );
+                    }
+                    return valid;
+                }    
+            } );
+            </script>
 <?php            
 
             $output = '';
@@ -181,7 +249,7 @@ if (!class_exists('curtain_models')) {
                 $output .= '</tr>';
             }
             $output .= '</tbody></table></div>';
-            $output .= '<button id="create-model">Create new model</button>';
+            $output .= '<button id="create-button">Create new model</button>';
 /*
             $output .= '<form method="post">';
             $output .= '<div class="wp-block-buttons">';
