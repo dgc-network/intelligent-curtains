2c13f70091d1a61a939caf7192644e5a52cc7818
Rover Chen <rover.k.chen@gmail.com>
2022-10-23 22:07:09 +0800

    var dialog, form,     emailRegex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,     name = $( "#name" ),     email = $( "#email" ),     password = $( "#password" ),     allFields = $( [] ).add( name ).add( email ).add( password ),     tips = $( ".validateTips" );
        dialog = $( "#dialog-form" ).dialog({
            //autoOpen: false,
            autoOpen: true,
            height: 400,
            width: 350,
            modal: true,
            buttons: {
                "Create": addUser,
                Cancel: function() {
                    dialog.dialog( "close" );
                }
            },
            close: function() {
                form[ 0 ].reset();
                allFields.removeClass( "ui-state-error" );
            }
        });
    
        //$( "#dialog-form" ).dialog( "open" );
    
        form = dialog.find( "form" ).on( "submit", function( event ) {
            event.preventDefault();
            addUser();
        });
    
        function updateTips( t ) {
            tips
                .text( t )
                .addClass( "ui-state-highlight" );
            setTimeout(function() {
                tips.removeClass( "ui-state-highlight", 1500 );
            }, 500 );
        }
    
        function checkLength( o, n, min, max ) {
            if ( o.val().length > max || o.val().length < min ) {
                o.addClass( "ui-state-error" );
                updateTips( "Length of " + n + " must be between " +
                    min + " and " + max + "." );
                return false;
            } else {
                return true;
            }
        }
    
        function checkRegexp( o, regexp, n ) {
            if ( !( regexp.test( o.val() ) ) ) {
                o.addClass( "ui-state-error" );
                updateTips( n );
                return false;
            } else {
                return true;
            }
        }
    
        function addUser() {
            var valid = true;
            allFields.removeClass( "ui-state-error" );
    
            valid = valid && checkLength( name, "username", 3, 16 );
            valid = valid && checkLength( email, "email", 6, 80 );
            valid = valid && checkLength( password, "password", 5, 16 );
    
            valid = valid && checkRegexp( name, /^[a-z]([0-9a-z_\s])+$/i, "Username may consist of a-z, 0-9, underscores, spaces and must begin with a letter." );
            valid = valid && checkRegexp( email, emailRegex, "eg. ui@jquery.com" );
            valid = valid && checkRegexp( password, /^([0-9a-zA-Z])+$/, "Password field only allow : a-z 0-9" );
    
            if ( valid ) {
                $( "#users tbody" ).append( "<tr>" +
                    "<td>" + name.val() + "</td>" +
                    "<td>" + email.val() + "</td>" +
                    "<td>" + password.val() + "</td>" +
                "</tr>" );
                dialog.dialog( "close" );
            }
            return valid;
        }
    
        $( "#create-model" ).button().on( "click", function() {
            dialog.dialog( "open" );
        });

--- includes/class-curtain-models.php
+++ includes/class-curtain-models.php
@@ -11,7 +11,7 @@ if (!class_exists('curtain_models')) {
          */
         public function __construct() {
             add_shortcode('curtain-model-list', __CLASS__ . '::list_curtain_models');
-            add_action( 'wp_enqueue_scripts', array( __CLASS__, 'enqueue_scripts' ) );
+            //add_action( 'wp_enqueue_scripts', array( __CLASS__, 'enqueue_scripts' ) );
             self::create_tables();
         }
 
@@ -87,6 +87,97 @@ if (!class_exists('curtain_models')) {
 ?>
             <script>
             $( function() {
+
+                var dialog, form,  
+    emailRegex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
+    name = $( "#name" ),
+    email = $( "#email" ),
+    password = $( "#password" ),
+    allFields = $( [] ).add( name ).add( email ).add( password ),
+    tips = $( ".validateTips" );
+
+    dialog = $( "#dialog-form" ).dialog({
+        //autoOpen: false,
+        autoOpen: true,
+        height: 400,
+        width: 350,
+        modal: true,
+        buttons: {
+            "Create": addUser,
+            Cancel: function() {
+                dialog.dialog( "close" );
+            }
+        },
+        close: function() {
+            form[ 0 ].reset();
+            allFields.removeClass( "ui-state-error" );
+        }
+    });
+
+    //$( "#dialog-form" ).dialog( "open" );
+
+    form = dialog.find( "form" ).on( "submit", function( event ) {
+        event.preventDefault();
+        addUser();
+    });
+
+    function updateTips( t ) {
+        tips
+            .text( t )
+            .addClass( "ui-state-highlight" );
+        setTimeout(function() {
+            tips.removeClass( "ui-state-highlight", 1500 );
+        }, 500 );
+    }
+
+    function checkLength( o, n, min, max ) {
+        if ( o.val().length > max || o.val().length < min ) {
+            o.addClass( "ui-state-error" );
+            updateTips( "Length of " + n + " must be between " +
+                min + " and " + max + "." );
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    function checkRegexp( o, regexp, n ) {
+        if ( !( regexp.test( o.val() ) ) ) {
+            o.addClass( "ui-state-error" );
+            updateTips( n );
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    function addUser() {
+        var valid = true;
+        allFields.removeClass( "ui-state-error" );
+
+        valid = valid && checkLength( name, "username", 3, 16 );
+        valid = valid && checkLength( email, "email", 6, 80 );
+        valid = valid && checkLength( password, "password", 5, 16 );
+
+        valid = valid && checkRegexp( name, /^[a-z]([0-9a-z_\s])+$/i, "Username may consist of a-z, 0-9, underscores, spaces and must begin with a letter." );
+        valid = valid && checkRegexp( email, emailRegex, "eg. ui@jquery.com" );
+        valid = valid && checkRegexp( password, /^([0-9a-zA-Z])+$/, "Password field only allow : a-z 0-9" );
+
+        if ( valid ) {
+            $( "#users tbody" ).append( "<tr>" +
+                "<td>" + name.val() + "</td>" +
+                "<td>" + email.val() + "</td>" +
+                "<td>" + password.val() + "</td>" +
+            "</tr>" );
+            dialog.dialog( "close" );
+        }
+        return valid;
+    }
+
+    $( "#create-model" ).button().on( "click", function() {
+        dialog.dialog( "open" );
+    });
+
 
             } );
             </script>
