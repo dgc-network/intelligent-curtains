dd37e2c89b8c7ddc69bc9e4ee28de80e40739647
Rover Chen <rover.k.chen@gmail.com>
2022-10-23 21:21:30 +0800

    custom-options-view

--- includes/class-curtain-models.php
+++ includes/class-curtain-models.php
@@ -17,7 +17,9 @@ if (!class_exists('curtain_models')) {
 
         function enqueue_scripts() {		
             wp_enqueue_script( 'custom-curtain-models', plugin_dir_url( __DIR__ ) . 'assets/js/custom-curtain-models.js', array( 'jquery' ), time(), true );
-        }    
+            wp_enqueue_style( 'jquery-ui' );  
+            wp_enqueue_style( 'demos-style' );  
+            }    
 
         function list_curtain_models() {
 ?>
