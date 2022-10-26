2c2ae413f058368013df13647283e6c41769d2f2
Rover Chen <rover.k.chen@gmail.com>
2022-10-26 21:15:21 +0800

    return 'You are not validated to read this page. Please check to the administrators.';

--- includes/class-curtain-agents.php
+++ includes/class-curtain-agents.php
@@ -26,9 +26,7 @@ if (!class_exists('curtain_agents')) {
                 $line_user_id = $_SESSION['line_user_id'];
                 global $wpdb;
                 $user = $wpdb->get_row( $wpdb->prepare( "SELECT * FROM {$wpdb->prefix}curtain_users WHERE line_user_id = %s AND user_role= %s", $line_user_id, 'admin' ), OBJECT );            
-                if (count($user) > 0) {
-                    //return 'Welcome '.$user->display_name;
-                } else {
+                if (count($user) == 0) {
                     return 'You are not validated to read this page. Please check to the administrators.';
                 }
             } else {
