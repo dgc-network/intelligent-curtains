8531c9e50f7b1ca2a3dfa404625119aa9e849a24
Rover Chen <rover.k.chen@gmail.com>
2022-10-26 15:37:51 +0800

    $_SESSION['line_user_id'])

--- includes/class-curtain-users.php
+++ includes/class-curtain-users.php
@@ -229,6 +229,19 @@ if (!class_exists('curtain_users')) {
 
         function list_curtain_users() {
 
+            if( isset($_SESSION['line_user_id']) ) {
+                $line_user_id = $_SESSION['line_user_id'];
+                global $wpdb;
+                $user = $wpdb->get_row( $wpdb->prepare( "SELECT * FROM {$wpdb->prefix}curtain_users WHERE line_user_id = %s AND user_role= %s", $line_user_id, 'admin' ), OBJECT );            
+                if (count($user) > 0) {
+                    return 'Welcome '.$user->display_name;
+                } else {
+                    return;
+                }
+            } else {
+                return;
+            }
+
             if( isset($_POST['_update']) ) {
                 $data=array();
                 $data['display_name']=$_POST['_display_name'];
