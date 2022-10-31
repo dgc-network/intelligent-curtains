6629c0bb4c7a849b73ce5421d9ec75d8d6bcf37e
Rover Chen <rover.k.chen@gmail.com>
2022-11-01 05:54:47 +0800

    $serial_number = new serial_number();

--- includes/class-serial-number.php
+++ includes/class-serial-number.php
@@ -11,8 +11,8 @@ if (!class_exists('serial_number')) {
          */
         public function __construct() {
             $serial_number = new serial_number();
-            //add_shortcode('serial-number-list', __CLASS__ . '::list_serial_number');
-            add_shortcode('serial-number-list', $serial_number->list_serial_number());
+            add_shortcode('serial-number-list', __CLASS__ . '::list_serial_number');
+            //add_shortcode('serial-number-list', $serial_number->list_serial_number());
             //add_action( 'init', array( __CLASS__, 'wpse16119876_init_session' ) );
             self::create_tables();
         }
