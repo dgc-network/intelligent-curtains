1aa15bd222e7a0929854b36475e0e0bf4e51e080
Rover Chen <rover.k.chen@gmail.com>
2022-11-10 10:33:10 +0800

    entityBody

--- includes/class-line-webhook.php
+++ includes/class-line-webhook.php
@@ -32,12 +32,14 @@ if (!class_exists('line_webhook')) {
         }
 */
         public function init() {
+/*
             $client = line_bot_sdk();
             foreach ($client->parseEvents() as $event) {
             }
         }
 
         public function init_backup() {
+*/
             //$client = self::line_bot_sdk();
             $client = line_bot_sdk();
             foreach ((array)$client->parseEvents() as $event) {
