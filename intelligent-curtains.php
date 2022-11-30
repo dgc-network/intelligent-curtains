f03a218335d166c24fa9a1554a37473279d3ff84
Rover Chen <rover.k.chen@gmail.com>
2022-12-01 06:49:06 +0800

    //Begin Really Simple SSL Server variable fix    $_SERVER["HTTPS"] = "on"; //END Really Simple SSL

--- intelligent-curtains.php
+++ intelligent-curtains.php
@@ -35,7 +35,7 @@ require_once MY_PLUGIN_DIR . 'includes/class-curtain-agents.php';
 require_once MY_PLUGIN_DIR . 'includes/class-curtain-orders.php';
 require_once MY_PLUGIN_DIR . 'includes/class-curtain-models.php';
 //require_once MY_PLUGIN_DIR . 'includes/class-curtain-users.php';
-require_once MY_PLUGIN_DIR . 'includes/class-serial-number.php';
+//require_once MY_PLUGIN_DIR . 'includes/class-serial-number.php';
 add_option('_service_page', 'service');
 add_option('_users_page', 'users');
 add_option('_line_account', 'https://line.me/ti/p/@490tjxdt');
