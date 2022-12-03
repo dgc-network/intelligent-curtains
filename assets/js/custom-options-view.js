7aaf2d3736853bd741d9e6ef9f9a3a0225d8354a
Rover Chen <rover.k.chen@gmail.com>
2022-12-03 08:35:21 +0800

    ajaxurl

--- assets/js/custom-options-view.js
+++ assets/js/custom-options-view.js
@@ -27,7 +27,7 @@ jQuery(document).ready(function($) {
         jQuery.ajax({
             type: 'POST',
             //url: '/wp-admin/admin-ajax.php',
-            url: 'admin-ajax.php',
+            url: ajaxurl,
             dataType: "json",
             data: {
                 'action': 'select_product_id',
