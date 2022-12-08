5c1c6ca35d7ecaf5224ab9eab6013884054fbd8d
Rover Chen <rover.k.chen@gmail.com>
2022-12-08 12:07:20 +0800

    $output = '<div id="dialog" title="Chat with '.$row->display_name.'">';

--- assets/js/custom-options-view.js
+++ assets/js/custom-options-view.js
@@ -185,12 +185,12 @@ jQuery(document).ready(function($) {
                 dialog.dialog( "close" );
             }
         },
-/*        
+        
         close: function() {
             form[ 0 ].reset();
             allFields.removeClass( "ui-state-error" );
         }
-*/        
+    
     });
 
     // From http://www.whatwg.org/specs/web-apps/current-work/multipage/states-of-the-type-attribute.html#e-mail-state-%28type=email%29
