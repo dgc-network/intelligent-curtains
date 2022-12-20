20c73e5cbbc4e68607892be6b4acd8725bcaee0b
Rover Chen <rover.k.chen@gmail.com>
2022-12-20 09:35:18 +0800

    chat_messages

--- uninstall.php
+++ uninstall.php
@@ -1,8 +1,8 @@
 <?php
 if( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) exit();
 global $wpdb;
-$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}orders" );
-$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}chat_messages" );
+//$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}orders" );
+//$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}chat_messages" );
 //$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}serial_number" );
 //$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}specification" );
 //$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}service_options" );
