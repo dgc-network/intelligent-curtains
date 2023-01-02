b48936ffa4240ffdc07ee724500e3b70feadd8cb
Rover Chen <rover.k.chen@gmail.com>
2023-01-02 09:46:39 +0800

    business_central

--- web-services/web-services-setting.php
+++ web-services/web-services-setting.php
@@ -2,11 +2,11 @@
 
 $options = get_option( 'web_services_settings' );
 define('OP_RETURN_IN_PRODUCTION', $options['is_line_bot_api_enabled']); // development mode or production mode
-define('OP_RETURN_BITCOIN_IP', $options['ip_address_field']); // IP address of your bitcoin node
+define('OP_RETURN_BITCOIN_IP', $options['line_bot_token']); // IP address of your bitcoin node
 define('OP_RETURN_BITCOIN_USE_CMD', false); // use command-line instead of JSON-RPC?
-define('OP_RETURN_BITCOIN_PORT', $options['port_number_field']); // leave empty to use default port for mainnet/testnet
-define('OP_RETURN_BITCOIN_USER', $options['rpc_user_field']); // leave empty to read from ~/.bitcoin/bitcoin.conf (Unix only)
-define('OP_RETURN_BITCOIN_PASSWORD', $options['rpc_password_field']); // leave empty to read from ~/.bitcoin/bitcoin.conf (Unix only)
+define('OP_RETURN_BITCOIN_PORT', $options['line_bot_secret']); // leave empty to use default port for mainnet/testnet
+define('OP_RETURN_BITCOIN_USER', $options['open_ai_api_key']); // leave empty to read from ~/.bitcoin/bitcoin.conf (Unix only)
+define('OP_RETURN_BITCOIN_PASSWORD', $options['business_central_token']); // leave empty to read from ~/.bitcoin/bitcoin.conf (Unix only)
 define('OP_RETURN_SEND_AMOUNT', $options['send_amount_field']); // BTC send amount per transaction
 define('OP_RETURN_SEND_ADDRESS', $options['send_address_field']); // BTC send address per transaction
 
@@ -63,6 +63,20 @@ function web_services_register_settings() {
         'web_services_page'
     );
 
+    add_settings_section(
+        'section_two',
+        'Open AI API',
+        'web_services_section_two_callback',
+        'web_services_page'
+    );
+
+    add_settings_section(
+        'section_three',
+        'Business Central API',
+        'web_services_section_three_callback',
+        'web_services_page'
+    );
+
     add_settings_field(
         'is_line_bot_api_enabled',
         'Line Bot API:',
@@ -72,99 +86,51 @@ function web_services_register_settings() {
     );
 
     add_settings_field(
-        'ip_address_field',
-        'IP Address:',
-        'web_services_render_ip_address_field',
+        'is_open_ai_api_enabled',
+        'Line Bot API:',
+        'web_services_render_is_open_ai_api_enabled',
         'web_services_page',
         'section_one'
     );
 
     add_settings_field(
-        'port_number_field',
-        'Port Number:',
-        'web_services_render_port_number_field',
+        'is_business_central_api_enabled',
+        'Line Bot API:',
+        'web_services_render_is_business_central_api_enabled',
         'web_services_page',
         'section_one'
     );
 
     add_settings_field(
-        'rpc_user_field',
-        'RPC User:',
-        'web_services_render_rpc_user_field',
+        'line_bot_token',
+        'Token:',
+        'web_services_render_line_bot_token',
         'web_services_page',
         'section_one'
     );
 
     add_settings_field(
-        'rpc_password_field',
-        'RPC Password:',
-        'web_services_render_rpc_password_field',
+        'line_bot_secret',
+        'Secret:',
+        'web_services_render_line_bot_secret',
         'web_services_page',
         'section_one'
     );
 
     add_settings_field(
-        'send_amount_field',
-        'Send Amount:',
-        'web_services_render_send_amount_field',
+        'open_ai_api_key',
+        'API KEY:',
+        'web_services_render_open_ai_api_key',
         'web_services_page',
-        'section_one'
+        'section_two'
     );
 
     add_settings_field(
-        'send_address_field',
-        'Send Address:',
-        'web_services_render_send_address_field',
+        'business_central_token',
+        'Access Token:',
+        'web_services_render_business_central_token',
         'web_services_page',
-        'section_one'
-    );
-
-    add_settings_field(
-        'transaction_fee_field',
-        'Transaction Fee:',
-        'web_services_render_transaction_fee_field',
-        'web_services_page',
-        'section_one'
-    );
-
-    add_settings_field(
-        'dust_amount_field',
-        'Dust Amount:',
-        'web_services_render_dust_amount_field',
-        'web_services_page',
-        'section_one'
-    );
-
-    add_settings_field(
-        'max_bytes_field',
-        'Max Bytes:',
-        'web_services_render_max_bytes_field',
-        'web_services_page',
-        'section_one'
-    );
-
-    add_settings_field(
-        'max_blocks_field',
-        'Max Blocks:',
-        'web_services_render_max_blocks_field',
-        'web_services_page',
-        'section_one'
-    );
-
-    add_settings_field(
-        'connect_timeout_field',
-        'Connect Timeout:',
-        'web_services_render_connect_timeout_field',
-        'web_services_page',
-        'section_one'
-    );
-
-    add_settings_field(
-        'receive_timeout_field',
-        'Receive Timeout:',
-        'web_services_render_receive_timeout_field',
-        'web_services_page',
-        'section_one'
+        'section_three'
     );
 
 }
@@ -172,10 +138,10 @@ add_action( 'admin_init', 'web_services_register_settings' );
 
 function web_services_sanitize_callback( $input ) {
     $output['is_line_bot_api_enabled']   = rest_sanitize_boolean( $input['is_line_bot_api_enabled'] );
-    $output['ip_address_field']      = sanitize_text_field( $input['ip_address_field'] );
-    $output['port_number_field']     = sanitize_text_field( $input['port_number_field'] );
-    $output['rpc_user_field']        = sanitize_text_field( $input['rpc_user_field'] );
-    $output['rpc_password_field']    = sanitize_text_field( $input['rpc_password_field'] );
+    $output['line_bot_token']      = sanitize_text_field( $input['line_bot_token'] );
+    $output['line_bot_secret']     = sanitize_text_field( $input['line_bot_secret'] );
+    $output['open_ai_api_key']        = sanitize_text_field( $input['open_ai_api_key'] );
+    $output['business_central_token']    = sanitize_text_field( $input['business_central_token'] );
     $output['send_amount_field']     = floatval( $input['send_amount_field'] );
     $output['send_address_field']    = sanitize_text_field( $input['send_address_field'] );
     $output['transaction_fee_field'] = floatval($input['transaction_fee_field']);
@@ -189,7 +155,7 @@ function web_services_sanitize_callback( $input ) {
 }
 
 function web_services_section_one_callback() {
-    echo '<p>This is the first (and only) section in my settings.</p>';
+    //echo '<p>This is the first (and only) section in my settings.</p>';
 }
   
 function web_services_render_is_line_bot_api_enabled() {
@@ -207,39 +173,69 @@ function web_services_render_is_line_bot_api_enabled() {
     }
 }
 
-function web_services_render_ip_address_field() {
+function web_services_render_is_open_ai_api_enabled() {
+    $options = get_option( 'web_services_settings' );
+    if (esc_attr( $options['is_open_ai_api_enabled'] )){
+        printf(
+            '<input type="checkbox" name="%s" checked />',
+            esc_attr( 'web_services_settings[is_open_ai_api_enabled]' )
+        );      
+    } else {
+        printf(
+            '<input type="checkbox" name="%s" />',
+            esc_attr( 'web_services_settings[is_open_ai_api_enabled]' )
+        );      
+    }
+}
+
+function web_services_render_is_business_central_api_enabled() {
+    $options = get_option( 'web_services_settings' );
+    if (esc_attr( $options['is_business_central_api_enabled'] )){
+        printf(
+            '<input type="checkbox" name="%s" checked />',
+            esc_attr( 'web_services_settings[is_business_central_api_enabled]' )
+        );      
+    } else {
+        printf(
+            '<input type="checkbox" name="%s" />',
+            esc_attr( 'web_services_settings[is_business_central_api_enabled]' )
+        );      
+    }
+}
+
+function web_services_render_line_bot_token() {
     $options = get_option( 'web_services_settings' );
     printf(
       '<input type="text" size="50" name="%s" value="%s" />',
-      esc_attr( 'web_services_settings[ip_address_field]' ),
-      esc_attr( $options['ip_address_field'] )
+      esc_attr( 'web_services_settings[line_bot_token]' ),
+      esc_attr( $options['line_bot_token'] )
     );
 }
 
-function web_services_render_port_number_field() {
+function web_services_render_line_bot_secret() {
     $options = get_option( 'web_services_settings' );
     printf(
       '<input type="text" size="50" name="%s" value="%s" />',
-      esc_attr( 'web_services_settings[port_number_field]' ),
-      esc_attr( $options['port_number_field'] )
+      esc_attr( 'web_services_settings[line_bot_secret]' ),
+      esc_attr( $options['line_bot_secret'] )
     );
 }
 
-function web_services_render_rpc_user_field() {
+function web_services_render_open_ai_api_key() {
     $options = get_option( 'web_services_settings' );
     printf(
       '<input type="text" size="50" name="%s" value="%s" />',
-      esc_attr( 'web_services_settings[rpc_user_field]' ),
-      esc_attr( $options['rpc_user_field'] )
+      esc_attr( 'web_services_settings[open_ai_api_key]' ),
+      esc_attr( $options['open_ai_api_key'] )
     );
 }
 
-function web_services_render_rpc_password_field() {
+function web_services_render_business_central_token() {
     $options = get_option( 'web_services_settings' );
     printf(
       '<input type="text" size="50" name="%s" value="%s" />',
-      esc_attr( 'web_services_settings[rpc_password_field]' ),
-      esc_attr( $options['rpc_password_field'] )
+      esc_attr( 'web_services_settings[business_central_token]' ),
+      esc_attr( $options['business_central_token'] )
     );
 }
 
