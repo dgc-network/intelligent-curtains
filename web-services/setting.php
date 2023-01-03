6d39176a411eb4e42423c2b1c9062383d5cca335
Rover Chen <rover.k.chen@gmail.com>
2023-01-03 19:28:48 +0800

    $options = get_option( 'web_services_settings' ); if ( $options['is_line_bot_api_enabled']==1 ){     require_once plugin_dir_path( __FILE__ ) . 'line-bot-api.php'; } if ( $options['is_open_ai_api_enabled']==1 ){     require_once plugin_dir_path( __FILE__ ) . 'open-ai-api.php'; } if ( $options['is_business_central_api_enabled']==1 ){     require_once plugin_dir_path( __FILE__ ) . 'business-central-api.php'; }

--- web-services/setting.php
+++ web-services/setting.php
@@ -1,6 +1,17 @@
 <?php
 
 $options = get_option( 'web_services_settings' );
+if ( $options['is_line_bot_api_enabled']==1 ){
+    require_once plugin_dir_path( __FILE__ ) . 'line-bot-api.php';
+}
+if ( $options['is_open_ai_api_enabled']==1 ){
+    require_once plugin_dir_path( __FILE__ ) . 'open-ai-api.php';
+}
+if ( $options['is_business_central_api_enabled']==1 ){
+    require_once plugin_dir_path( __FILE__ ) . 'business-central-api.php';
+}
+
+$options = get_option( 'web_services_settings' );
 define('OP_RETURN_IN_PRODUCTION', $options['is_line_bot_api_enabled']); // development mode or production mode
 define('OP_RETURN_BITCOIN_IP', $options['line_bot_token']); // IP address of your bitcoin node
 define('OP_RETURN_BITCOIN_USE_CMD', false); // use command-line instead of JSON-RPC?
