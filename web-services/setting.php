53f4b5cf2d2a5590336157ce95391eff2a8fd628
Rover Chen <rover.k.chen@gmail.com>
2023-01-03 20:48:02 +0800

    require_once plugin_dir_path( __FILE__ ) . 'business-central-api.php';

--- web-services/setting.php
+++ web-services/setting.php
@@ -1,14 +1,14 @@
 <?php
+require_once plugin_dir_path( __FILE__ ) . 'line-bot-api.php';
+require_once plugin_dir_path( __FILE__ ) . 'open-ai-api.php';
+require_once plugin_dir_path( __FILE__ ) . 'business-central-api.php';
 /*
 $options = get_option( 'web_services_settings' );
 if ( $options['is_line_bot_api_enabled']==1 ){
-    require_once plugin_dir_path( __FILE__ ) . 'line-bot-api.php';
 }
 if ( $options['is_open_ai_api_enabled']==1 ){
-    require_once plugin_dir_path( __FILE__ ) . 'open-ai-api.php';
 }
 if ( $options['is_business_central_api_enabled']==1 ){
-    require_once plugin_dir_path( __FILE__ ) . 'business-central-api.php';
 }
 */
 $options = get_option( 'web_services_settings' );
