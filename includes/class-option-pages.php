5215a15456331db9269707f549f405e8f3a3977d
Rover Chen <rover.k.chen@gmail.com>
2023-01-07 20:05:22 +0800

    get_permalink

--- includes/class-option-pages.php
+++ includes/class-option-pages.php
@@ -40,7 +40,7 @@ if (!class_exists('option_pages')) {
             $this->insert_option_page(
                 array(
                     'service_option_title' => $title_of_the_page,
-                    'service_option_link' => get_page_link($title_of_the_page),
+                    'service_option_link' => get_permalink($title_of_the_page),
                     'service_option_category' => $category,
                 )
             );
