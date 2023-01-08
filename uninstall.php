3aefbb79ec96412cab299da795ad2b16b25db1bb
Rover Chen <rover.k.chen@gmail.com>
2023-01-08 20:34:03 +0800

    class="wp-block-button__link" type="submit"

--- uninstall.php
+++ uninstall.php
@@ -1,7 +1,7 @@
 <?php
 if( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) exit();
 global $wpdb;
-$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}service_links" );
+//$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}service_links" );
 //$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}order_items" );
 //$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}serial_number" );
 //$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}specification" );
