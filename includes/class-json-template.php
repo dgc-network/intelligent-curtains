49da9d2cfd244d25242460d7d9d2eda24b9f1edc
Rover Chen <rover.k.chen@gmail.com>
2023-01-13 07:36:51 +0800

    '.$row->json_template_text.'

--- includes/class-json-template.php
+++ includes/class-json-template.php
@@ -3,23 +3,23 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-if (!class_exists('curtain_remotes')) {
-    class curtain_remotes {
+if (!class_exists('json_templates')) {
+    class json_templates {
         private $_wp_page_title;
         private $_wp_page_postid;
         /**
          * Class constructor
          */
         public function __construct() {
-            $this->_wp_page_title = 'Remotes';
+            $this->_wp_page_title = 'Templates';
             $this->_wp_page_postid = get_page_by_title($this->_wp_page_title)->ID;
             $wp_pages = new wp_pages();
-            $wp_pages->create_page($this->_wp_page_title, '[curtain-remote-list]');            
-            add_shortcode( 'curtain-remote-list', array( $this, 'list_curtain_remotes' ) );
+            $wp_pages->create_page($this->_wp_page_title, '[json-template-list]');            
+            add_shortcode( 'json-template-list', array( $this, 'list_json_templates' ) );
             $this->create_tables();
         }
 
-        public function list_curtain_remotes() {
+        public function list_json_templates() {
             global $wpdb;
             $wp_pages = new wp_pages();
 
@@ -37,43 +37,43 @@ if (!class_exists('curtain_remotes')) {
             }
 
             if( isset($_POST['_create']) ) {
-                $curtain_remotes->insert_curtain_remote(
+                $curtain_remotes->insert_json_template(
                     array(
-                        'curtain_remote_name'=>$_POST['_curtain_remote_name'],
-                        'curtain_remote_price'=>$_POST['_curtain_remote_price']
+                        'json_template_title'=>$_POST['_json_template_title'],
+                        'json_template_text'=>$_POST['_json_template_text']
                     )
                 );
             }
             
             if( isset($_POST['_update']) ) {
-                $curtain_remotes->update_curtain_remotes(
+                $curtain_remotes->update_json_templates(
                     array(
-                        'curtain_remote_name'=>$_POST['_curtain_remote_name'],
-                        'curtain_remote_price'=>$_POST['_curtain_remote_price']
+                        'json_template_title'=>$_POST['_json_template_title'],
+                        'json_template_text'=>$_POST['_json_template_text']
                     ),
                     array(
-                        'curtain_remote_id'=>$_POST['_curtain_remote_id'],
+                        'json_template_id'=>$_POST['_json_template_id'],
                     )
                 );
                 ?><script>window.location.replace("?_update=");</script><?php
             }
 
             if( isset($_GET['_delete']) ) {
-                $curtain_remotes->delete_curtain_remotes(
+                $curtain_remotes->delete_json_templates(
                     array(
-                        'curtain_remote_id'=>$_GET['_delete']
+                        'json_template_id'=>$_GET['_delete']
                     )
                 );
             }
 
             if( isset($_POST['_where']) ) {
                 $where='"%'.$_POST['_where'].'%"';
-                $results = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}curtain_remotes WHERE curtain_remote_name LIKE {$where}", OBJECT );
+                $results = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}json_templates WHERE curtain_remote_name LIKE {$where}", OBJECT );
                 unset($_POST['_where']);
             } else {
-                $results = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}curtain_remotes", OBJECT );
+                $results = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}json_templates", OBJECT );
             }
-            $output  = '<h2>Curtain Remotes</h2>';
+            $output  = '<h2>json template</h2>';
             $output .= '<div style="display: flex; justify-content: space-between; margin: 5px;">';
             $output .= '<div>';
             $output .= '<form method="post">';
@@ -92,8 +92,8 @@ if (!class_exists('curtain_remotes')) {
             $output .= '<table id="remotes" class="ui-widget ui-widget-content">';
             $output .= '<thead><tr class="ui-widget-header ">';
             $output .= '<th></th>';
-            $output .= '<th>remote</th>';
-            $output .= '<th>price</th>';
+            $output .= '<th>title</th>';
+            $output .= '<th>json</th>';
             $output .= '<th>update_time</th>';
             $output .= '<th></th>';
             $output .= '</tr></thead>';
@@ -101,13 +101,13 @@ if (!class_exists('curtain_remotes')) {
             foreach ( $results as $index=>$result ) {
                 $output .= '<tr>';
                 $output .= '<td style="text-align: center;">';
-                $output .= '<span id="btn-edit-'.$result->curtain_remote_id.'"><i class="fa-regular fa-pen-to-square"></i></span>';
+                $output .= '<span id="btn-edit-'.$result->json_template_id.'"><i class="fa-regular fa-pen-to-square"></i></span>';
                 $output .= '</td>';
-                $output .= '<td>'.$result->curtain_remote_name.'</td>';
-                $output .= '<td>'.$result->curtain_remote_price.'</td>';
+                $output .= '<td>'.$result->json_template_title.'</td>';
+                $output .= '<td>'.$result->json_template_text.'</td>';
                 $output .= '<td>'.wp_date( get_option('date_format'), $result->update_timestamp ).' '.wp_date( get_option('time_format'), $result->update_timestamp ).'</td>';
                 $output .= '<td style="text-align: center;">';
-                $output .= '<span id="btn-del-'.$result->curtain_remote_id.'"><i class="fa-regular fa-trash-can"></i></span>';
+                $output .= '<span id="btn-del-'.$result->json_template_id.'"><i class="fa-regular fa-trash-can"></i></span>';
                 $output .= '</td>';
                 $output .= '</tr>';
             }
@@ -115,15 +115,16 @@ if (!class_exists('curtain_remotes')) {
 
             if( isset($_GET['_edit']) ) {
                 $_id = $_GET['_edit'];
-                $row = $wpdb->get_row( "SELECT * FROM {$wpdb->prefix}curtain_remotes WHERE curtain_remote_id={$_id}", OBJECT );
-                $output .= '<div id="dialog" title="Curtain remote update">';
+                $row = $wpdb->get_row( "SELECT * FROM {$wpdb->prefix}json_templates WHERE json_template_id={$_id}", OBJECT );
+                $output .= '<div id="dialog" title="template update">';
                 $output .= '<form method="post">';
                 $output .= '<fieldset>';
-                $output .= '<input type="hidden" value="'.$row->curtain_remote_id.'" name="_curtain_remote_id">';
-                $output .= '<label for="curtain-remote-name">Remote Name</label>';
-                $output .= '<input type="text" name="_curtain_remote_name" value="'.$row->curtain_remote_name.'" id="curtain-remote-name" class="text ui-widget-content ui-corner-all">';
-                $output .= '<label for="curtain-remote-price">Remote Price</label>';
-                $output .= '<input type="text" name="_curtain_remote_price" value="'.$row->curtain_remote_price.'" id="curtain-remote-price" class="text ui-widget-content ui-corner-all">';
+                $output .= '<input type="hidden" value="'.$row->json_template_id.'" name="_json_template_id">';
+                $output .= '<label for="json-template-title">Template Title</label>';
+                $output .= '<input type="text" name="_json_template_title" value="'.$row->json_template_title.'" id="json-template-title" class="text ui-widget-content ui-corner-all">';
+                $output .= '<label for="json-template-text">JSON</label>';
+                $output .= '<textarea class="chatboxtextarea" name="_json_template_text">'.$row->json_template_text.'</textarea>';
+                //$output .= '<input type="text" name="_json_template_text" value="'.$row->json_template_text.'" id="json-template-text" class="text ui-widget-content ui-corner-all">';
                 $output .= '</fieldset>';
                 $output .= '<input class="wp-block-button__link" type="submit" value="Update" name="_update">';
                 $output .= '</form>';
@@ -131,13 +132,14 @@ if (!class_exists('curtain_remotes')) {
             }
 
             if( isset($_POST['_add']) ) {
-                $output .= '<div id="dialog" title="Create new remote">';
+                $output .= '<div id="dialog" title="Create new template">';
                 $output .= '<form method="post">';
                 $output .= '<fieldset>';
-                $output .= '<label for="curtain-remote-name">Remote Name</label>';
-                $output .= '<input type="text" name="_curtain_remote_name" id="curtain-remote-name" class="text ui-widget-content ui-corner-all">';
-                $output .= '<label for="curtain-remote-price">Remote Price</label>';
-                $output .= '<input type="text" name="_curtain_remote_price" id="curtain-remote-price" class="text ui-widget-content ui-corner-all">';
+                $output .= '<label for="json-template-title">Template Title</label>';
+                $output .= '<input type="text" name="_json_template_title" id="json-template-title" class="text ui-widget-content ui-corner-all">';
+                $output .= '<label for="json-template-text">JSON</label>';
+                $output .= '<textarea class="chatboxtextarea" name="_json_template_text"></textarea>';
+                //$output .= '<input type="text" name="_json_template_text" id="json-template-text" class="text ui-widget-content ui-corner-all">';
                 $output .= '</fieldset>';
                 $output .= '<input class="wp-block-button__link" type="submit" value="Create" name="_create">';
                 $output .= '</form>';
@@ -146,55 +148,32 @@ if (!class_exists('curtain_remotes')) {
             return $output;
         }
 
-        public function insert_curtain_remote($data=[]) {
+        public function insert_json_template($data=[]) {
             global $wpdb;
-            $table = $wpdb->prefix.'curtain_remotes';
+            $table = $wpdb->prefix.'json_templates';
             $data['create_timestamp'] = time();
             $data['update_timestamp'] = time();
             $wpdb->insert($table, $data);        
             return $wpdb->insert_id;
         }
 
-        public function update_curtain_remotes($data=[], $where=[]) {
+        public function update_json_templates($data=[], $where=[]) {
             global $wpdb;
-            $table = $wpdb->prefix.'curtain_remotes';
+            $table = $wpdb->prefix.'json_templates';
             $data['update_timestamp'] = time();
             $wpdb->update($table, $data, $where);
         }
 
-        public function delete_curtain_remotes($where=[]) {
+        public function delete_json_templates($where=[]) {
             global $wpdb;
-            $table = $wpdb->prefix.'curtain_remotes';
+            $table = $wpdb->prefix.'json_templates';
             $wpdb->delete($table, $where);
         }
 
-        public function get_name( $_id=0 ) {
+        public function get_json( $_id=0 ) {
             global $wpdb;
-            $row = $wpdb->get_row( $wpdb->prepare( "SELECT * FROM {$wpdb->prefix}curtain_remotes WHERE curtain_remote_id = %d", $_id ), OBJECT );
-            return $row->curtain_remote_name;
-        }
-
-        public function get_price( $_id=0 ) {
-            global $wpdb;
-            $row = $wpdb->get_row( $wpdb->prepare( "SELECT * FROM {$wpdb->prefix}curtain_remotes WHERE curtain_remote_id = %d", $_id ), OBJECT );
-            return $row->curtain_remote_price;
-        }
-
-        public function select_options( $_id=0 ) {
-            global $wpdb;
-            $output = '<option value="0">-- Select an option --</option>';
-            $results = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}curtain_remotes", OBJECT );
-            foreach ($results as $index => $result) {
-                if ( $result->curtain_remote_id == $_id ) {
-                    $output .= '<option value="'.$result->curtain_remote_id.'" selected>';
-                } else {
-                    $output .= '<option value="'.$result->curtain_remote_id.'">';
-                }
-                $output .= $result->curtain_remote_name;
-                $output .= '</option>';        
-            }
-            $output .= '<option value="0">-- Remove this --</option>';
-            return $output;
+            $row = $wpdb->get_row( $wpdb->prepare( "SELECT * FROM {$wpdb->prefix}json_templates WHERE json_template_id = %d OR json_template_title = %s", $_id, $_id ), OBJECT );
+            return $row->json_template_text;
         }
 
         public function create_tables() {
@@ -202,16 +181,16 @@ if (!class_exists('curtain_remotes')) {
             $charset_collate = $wpdb->get_charset_collate();
             require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
         
-            $sql = "CREATE TABLE `{$wpdb->prefix}curtain_remotes` (
-                curtain_remote_id int NOT NULL AUTO_INCREMENT,
-                curtain_remote_name varchar(50),
-                curtain_remote_price decimal(10,2),
+            $sql = "CREATE TABLE `{$wpdb->prefix}json_templates` (
+                json_template_id int NOT NULL AUTO_INCREMENT,
+                json_template_title varchar(20),
+                json_template_text text,
                 create_timestamp int(10),
                 update_timestamp int(10),
-                PRIMARY KEY (curtain_remote_id)
+                PRIMARY KEY (json_template_id)
             ) $charset_collate;";
             dbDelta($sql);
         }
     }
-    $my_class = new curtain_remotes();
+    $my_class = new json_templates();
 }
