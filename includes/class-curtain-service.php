1e2608a12507883d97e970af6132c93527abb439
Rover Chen <rover.k.chen@gmail.com>
2023-03-16 14:28:12 +0800

    $curtain_agents

--- includes/class-curtain-service.php
+++ includes/class-curtain-service.php
@@ -82,10 +82,13 @@ if (!class_exists('curtain_service')) {
 
                         $output = '<div style="text-align:center;">';
                         $output .= '<br>chat_user_id: '.$_POST['_chat_user_id'];
+                        $output .= '<br>chat_display_name: '.$user->display_name;
                         $output .= '<br>curtain_agent_id: '.$_POST['_curtain_agent_id'];
+                        $output .= '<br>curtain_agent: '.$curtain_agents->get_name($_POST['_curtain_agent_id']);
                         $results = $wpdb->get_results( $wpdb->prepare( "SELECT * FROM {$wpdb->prefix}agent_operators WHERE curtain_agent_id = %d", $_POST['_curtain_agent_id'] ), OBJECT );
                         foreach ( $results as $result ) {
                             $author_obj = get_user_by('id', $result->curtain_user_id);
+                            $output .= '<br>';
                             $output .= '<br>to_agent_id: '.$result->curtain_user_id;
                             $output .= '<br>to_display_name: '.$author_obj->display_name;
                             $output .= '<br>to_line_id: '.get_user_meta($result->curtain_user_id, 'line_user_id', TRUE);
