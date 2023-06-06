2f512f2288860279291c04fb64383e55393d245a
Rover Chen <rover.k.chen@gmail.com>
2023-06-06 20:56:58 +0800

    $output .= '<label id="curtain-height-label" for="curtain-height">Height: min('.$curtain_categories->get_min_height($row->curtain_category_id).'),max('.$curtain_categories->get_max_height($row->curtain_category_id).')</label>';                 $output .= '<input type="text" name="_curtain_height" value="'.$row->curtain_height.'" id="curtain-height" class="text ui-widget-content ui-corner-all">';

--- includes/class-curtain-orders.php
+++ includes/class-curtain-orders.php
@@ -471,8 +471,10 @@ if (!class_exists('curtain_orders')) {
 
                 $output .= '<label id="curtain-width-label" for="curtain-width">Width: min('.$curtain_categories->get_min_width($row->curtain_category_id).'),max('.$curtain_categories->get_max_width($row->curtain_category_id).')</label>';
                 $output .= '<input type="text" name="_curtain_width" value="'.$row->curtain_width.'" id="curtain-width" class="text ui-widget-content ui-corner-all">';
-                $output .= '<label id="curtain-height-label" for="curtain-height">Height: min('.$curtain_categories->get_min_height($row->curtain_category_id).'),max('.$curtain_categories->get_max_height($row->curtain_category_id).')</label>';
-                $output .= '<input type="text" name="_curtain_height" value="'.$row->curtain_height.'" id="curtain-height" class="text ui-widget-content ui-corner-all">';
+                if ($row->curtain_category_id!=1) {
+                    $output .= '<label id="curtain-height-label" for="curtain-height">Height: min('.$curtain_categories->get_min_height($row->curtain_category_id).'),max('.$curtain_categories->get_max_height($row->curtain_category_id).')</label>';
+                    $output .= '<input type="text" name="_curtain_height" value="'.$row->curtain_height.'" id="curtain-height" class="text ui-widget-content ui-corner-all">';    
+                }
 
                 $output .= '<label for="order_item_qty">QTY</label>';
                 $output .= '<input type="text" name="_shopping_item_qty" value="'.$row->order_item_qty.'" id="order_item_qty" class="text ui-widget-content ui-corner-all">';
@@ -495,16 +497,7 @@ if (!class_exists('curtain_orders')) {
                 $output .= '<select name="_curtain_remote_id" id="select-remote-id">'.$curtain_remotes->select_options().'</select>';
                 $output .= '<label for="select-specification-id">Specification</label>';
                 $output .= '<select name="_curtain_specification_id" id="select-specification-id">'.$curtain_specifications->select_options().'</select>';
-/*
-                $output .= '<label for="curtain-dimension">Dimension</label>';
-                $output .= '<div style="display: flex;">';
-                $output .= '<span>Width</span>';
-                $output .= '<input type="text" name="_curtain_width" id="curtain-width" class="text ui-widget-content ui-corner-all">';
-                $output .= '<span>x</span>';
-                $output .= '<input type="text" name="_curtain_height" id="curtain-height" class="text ui-widget-content ui-corner-all">';
-                $output .= '<span>Height</span>';
-                $output .= '</div>';
-*/
+
                 $output .= '<label id="curtain-width-label" for="curtain-width">Width: min(),max()</label>';
                 $output .= '<input type="text" name="_curtain_width" id="curtain-width" class="text ui-widget-content ui-corner-all">';
                 $output .= '<label id="curtain-height-label" for="curtain-height">Height: min(),max()</label>';
