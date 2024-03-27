1609bced12bdb568f1c36c0d81b4a86c02580900
Rover Chen <rover.k.chen@gmail.com>
2024-03-27 19:55:40 +0800

    if (/Mobi/.test(navigator.userAgent)) {             // User is on a mobile device             $('.mobile-content').show();         } else {             // User is not on a mobile device, send one-time password via Line             $('.desktop-content').show();         }

--- includes/class-curtain-service.php
+++ includes/class-curtain-service.php
@@ -319,8 +319,8 @@ if (!class_exists('curtain_service')) {
             } else {
 
                 /** Did not login system yet */
-                user_did_not_login_yet();
-/*                
+                //user_did_not_login_yet();
+
                 if( isset($_GET['_id']) ) {
                     // Using Line User ID to register and login into the system
                     $array = get_users( array( 'meta_value' => $_GET['_id'] ));
@@ -363,7 +363,6 @@ if (!class_exists('curtain_service')) {
                     $output .= '</div>';
                     return $output;        
                 }
-*/            
             }
         }
 /*
