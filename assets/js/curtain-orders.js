430df4213c648a6e331d6f7a2cb0966436369312
Rover Chen <rover.k.chen@gmail.com>
2024-06-24 16:58:13 +0800

    curtain_agent_id

--- assets/js/curtain-orders.js
+++ assets/js/curtain-orders.js
@@ -422,6 +422,7 @@ jQuery(document).ready(function($) {
 
                 $('[id^="account-receivable-"]').on("click", function () {
                     const curtain_agent_id = this.id.substring(19);
+                    alert('agent_id'.curtain_agent_id)
                     $.ajax({
                         url: ajax_object.ajax_url,
                         type: 'post',
