72a2051f33894e46ac7604f214f35530afbc75c8
Rover Chen <rover.k.chen@gmail.com>
2024-08-14 09:17:50 +0800

    require_once plugin_dir_path( __FILE__ ) . 'includes/class-curtain-serials.php';

--- intelligent-curtains.php
+++ intelligent-curtains.php
@@ -64,6 +64,7 @@ require_once plugin_dir_path( __FILE__ ) . 'includes/class-curtain-agents.php';
 require_once plugin_dir_path( __FILE__ ) . 'includes/class-order-status.php';
 require_once plugin_dir_path( __FILE__ ) . 'includes/class-login-users.php';
 require_once plugin_dir_path( __FILE__ ) . 'includes/class-curtain-serials.php';
+require_once plugin_dir_path( __FILE__ ) . 'includes/class-product-items.php';
 
 require_once plugin_dir_path( __FILE__ ) . 'includes/general-helps.php';
 require_once plugin_dir_path( __FILE__ ) . 'includes/class-service-links.php';
