6e35de77d2fd7a837509a6c24238ce3064a53045
roverchen <rover.k.chen@gmail.com>
2024-12-06 17:06:08 +0800

    'type'    => 'NUMERIC'

--- includes/class-curtain-categories.php
+++ includes/class-curtain-categories.php
@@ -7,7 +7,6 @@ if (!class_exists('curtain_categories')) {
     class curtain_categories {
 
         public function __construct() {
-
             add_shortcode( 'curtain-category-list', array( $this, 'display_shortcode' ) );
             //add_action( 'init', array( $this, 'register_curtain_category_post_type' ) );
             add_action( 'wp_ajax_get_curtain_category_dialog_data', array( $this, 'get_curtain_category_dialog_data' ) );
@@ -16,7 +15,6 @@ if (!class_exists('curtain_categories')) {
             add_action( 'wp_ajax_nopriv_set_curtain_category_dialog_data', array( $this, 'set_curtain_category_dialog_data' ) );
             add_action( 'wp_ajax_del_curtain_category_dialog_data', array( $this, 'del_curtain_category_dialog_data' ) );
             add_action( 'wp_ajax_nopriv_del_curtain_category_dialog_data', array( $this, 'del_curtain_category_dialog_data' ) );
-
         }
 
         function register_curtain_category_post_type() {
@@ -131,15 +129,20 @@ if (!class_exists('curtain_categories')) {
             $is_specification_checked = ($is_specification == 1) ? 'checked' : '';
             $is_height = get_post_meta($curtain_category_id, 'is_height', true);
             $is_height_checked = ($is_height == 1) ? 'checked' : '';
+            $category_disabled = get_post_meta($curtain_category_id, 'category_disabled', true);
+            $is_disabled = ($category_disabled == 1) ? 'checked' : '';
             ?>
             <fieldset>
+                <div>
+                    <input type="checkbox" id="category-disabled" style="display:inline-block; width:5%; " <?php echo $is_disabled;?> />
+                    <label for="category-disabled"><?php echo __( 'Category disabled.', 'your-text-domain' );?></label>
+                </div>
                 <input type="hidden" id="curtain-category-id" value="<?php echo esc_attr($curtain_category_id);?>" />
                 <label for="curtain-category-title"><?php echo __( '窗簾類別', 'your-text-domain' );?></label>
                 <input type="text" id="curtain-category-title" value="<?php echo esc_html($curtain_category_title);?>" class="text ui-widget-content ui-corner-all" />
 
                 <input type="checkbox" id="is-specification" style="display:inline-block; width:5%; " <?php echo $is_specification_checked;?> /> Hide the Specification.
                 <div>
-                    <input type="checkbox" id="hide-width" style="display:inline-block; width:5%; " /> Hide the Width.
                     <div id="show-width">
                         <input type="text" id="curtain-min-width" value="<?php echo esc_html($curtain_min_width);?>" style="display:inline-block; width:25%;" /> cm ~ 
                         <input type="text" id="curtain-max-width" value="<?php echo esc_html($curtain_max_width);?>" style="display:inline-block; width:25%;" /> cm
@@ -152,7 +155,6 @@ if (!class_exists('curtain_categories')) {
                         <input type="text" id="curtain-max-height" value="<?php echo esc_html($curtain_max_height);?>" style="display:inline-block; width:25%;" /> cm
                     </div>
                 </div>
-                <input type="checkbox" id="allow-parts" style="display:inline-block; width:5%; " /> To be the parts for Sub Items.<br>
             </fieldset>
             <?php
             return ob_get_clean();
@@ -173,21 +175,28 @@ if (!class_exists('curtain_categories')) {
             $response = array();
             if( isset($_POST['_curtain_category_id']) ) {
                 // Update the meta data
-                $curtain_category_id = sanitize_text_field($_POST['_curtain_category_id']);
-                update_post_meta( $curtain_category_id, 'curtain_min_width', sanitize_text_field($_POST['_curtain_min_width']));
-                update_post_meta( $curtain_category_id, 'curtain_max_width', sanitize_text_field($_POST['_curtain_max_width']));
-                update_post_meta( $curtain_category_id, 'curtain_min_height', sanitize_text_field($_POST['_curtain_min_height']));
-                update_post_meta( $curtain_category_id, 'curtain_max_height', sanitize_text_field($_POST['_curtain_max_height']));
-                update_post_meta( $curtain_category_id, 'is_specification', sanitize_text_field($_POST['_is_specification']));
-                update_post_meta( $curtain_category_id, 'is_height', sanitize_text_field($_POST['_is_height']));
+                $curtain_category_id = (isset($_POST['_curtain_category_id'])) ? sanitize_text_field($_POST['_curtain_category_id']) : 0;
+                $curtain_category_title = (isset($_POST['_curtain_category_title'])) ? sanitize_text_field($_POST['_curtain_category_title']) : '';
+                $curtain_min_width = (isset($_POST['_curtain_min_width'])) ? sanitize_text_field($_POST['_curtain_min_width']) : 0;
+                $curtain_max_width = (isset($_POST['_curtain_max_width'])) ? sanitize_text_field($_POST['_curtain_max_width']) : 0;
+                $curtain_min_height = (isset($_POST['_curtain_min_height'])) ? sanitize_text_field($_POST['_curtain_min_height']) : 0;
+                $curtain_max_height = (isset($_POST['_curtain_max_height'])) ? sanitize_text_field($_POST['_curtain_max_height']) : 0;
+                $is_specification = (isset($_POST['_is_specification'])) ? sanitize_text_field($_POST['_is_specification']) : 0;
+                $is_height = (isset($_POST['_is_height'])) ? sanitize_text_field($_POST['_is_height']) : 0;
+                $category_disabled = (isset($_POST['_category_disabled'])) ? sanitize_text_field($_POST['_category_disabled']) : 0;
+                update_post_meta($curtain_category_id, 'curtain_min_width', $curtain_min_width);
+                update_post_meta($curtain_category_id, 'curtain_max_width', $curtain_max_width);
+                update_post_meta($curtain_category_id, 'curtain_min_height', $curtain_min_height);
+                update_post_meta($curtain_category_id, 'curtain_max_height', $curtain_max_height);
+                update_post_meta($curtain_category_id, 'is_specification', $is_specification);
+                update_post_meta($curtain_category_id, 'is_height', $is_height);
+                update_post_meta($curtain_category_id, 'category_disabled', $category_disabled);
                 // Update the post title
-                if (isset($_POST['_curtain_category_title'])) {
-                    $updated_post = array(
-                        'ID'         => $curtain_category_id,
-                        'post_title' => sanitize_text_field($_POST['_curtain_category_title']),
-                    );
-                    wp_update_post($updated_post);
-                }
+                $updated_post = array(
+                    'ID'         => $curtain_category_id,
+                    'post_title' => $curtain_category_title,
+                );
+                wp_update_post($updated_post);
             } else {
                 $current_user_id = get_current_user_id();
                 $new_post = array(
@@ -215,6 +224,14 @@ if (!class_exists('curtain_categories')) {
             $args = array(
                 'post_type'      => 'curtain-category',
                 'posts_per_page' => -1,
+                'meta_query' => array(
+                    array(
+                        'key'   => 'category_disabled',
+                        'value' => 1,
+                        'compare' => '!=',
+                        'type'    => 'NUMERIC',
+                    )
+                ),
             );
             $query = new WP_Query($args);
 
